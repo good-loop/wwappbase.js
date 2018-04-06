9c9f00c2b0a496e3049236aa8a965e023462865e
Daniel Winterstein <daniel@winterwell.com>
2018-04-06 17:18:33 +0100

    sync

--- src/wwappbase.js
+++
@@ -1,7 +0,0 @@
-import DataStore from './plumbing/DataStore';
-import Roles from './plumbing/Roles';
-import Settings from './plumbing/Settings';
-import AccountMenu from './components/AccountMenu';
-import AccountPage from './components/AccountPage';
-
-export {DataStore, Roles, Settings, AccountMenu, AccountPage}
