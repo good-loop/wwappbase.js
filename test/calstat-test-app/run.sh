44b47ba819ef4435a80ac795a03360f544a37f93
DA <daniel.appel.winterwell@gmail.com>
2018-04-12 16:16:25 +0100

    removed the calstat-test-app dir and added a smaller base for an actual test-app

--- test/calstat-test-app/run.sh
+++
@@ -1,1 +0,0 @@
-java -ea -cp .:lib/* com.winterwell.calstat.CalstatMain
--- test/calstat-test-app/src-js/plumbing/ActionMan.js
+++
@@ -1,7 +0,0 @@
-
-import ServerIO from './ServerIO';
-import {DataStore, ActionMan} from './wwappbase-test-output.js';
-import {assert} from 'sjtest';
-import _ from 'lodash';
-
-export default ActionMan;
