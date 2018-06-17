e82962a3c43cf57696b628555ef6a9abdb60e2cd
Daniel Winterstein <daniel@sodash.com>
2018-06-17 09:18:52 +0100

    folder standardisation refactor

--- project/webpack.config.js
+++ project/webpack.config.js
@@ -2,7 +2,9 @@ var path = require('path');
 var webpack = require('webpack');
 
 module.exports = {
-  entry: ["babel-polyfill", './src/app.jsx'],
+	entry: [
+		"babel-polyfill", './src/js/app.jsx'
+	],
   output: { path: __dirname, filename: './web/build/js/bundle.js' },
   devtool: 'source-map',
   resolve: {
