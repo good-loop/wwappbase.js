905fda0c1ed919d8d388da480f5279e866fe050e
Daniel Winterstein <daniel@winterwell.com>
2018-07-27 11:41:00 +0100

    Revert "bob auto build"
    This reverts commit 2cb06c447cbb374657b95ecb081b39d8b4157cbd.

---
+++ base/components/NavBar.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import AccountMenu from './AccountMenu';
+import C from '../CBase';
+import Roles from '../Roles';
+
+/**
+ * 
+ * @param {String} currentPage The current page
+ * @param {String[]} pages
+ */
+const NavBar = ({currentPage, pages, children}) => {
+	// make the page links
+	let pageLinks = pages.map( p => <NavLink currentPage={currentPage} targetPage={p} key={'li_'+p} /> );
+	return (
+		<nav className="navbar navbar-fixed-top navbar-inverse">
+			<div className="container">
+				<div className="navbar-header" title="Dashboard">
+					<button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+						<span className="sr-only">Toggle navigation</span>
+						<span className="icon-bar" /><span className="icon-bar" /><span className="icon-bar" />
+					</button>
+					<a className="" href="#dashboard">
+						<img className='logo-xlarge' alt={C.app.name} src={C.app.homeLogo || C.app.logo} />
+					</a>
+				</div>
+				<div id="navbar" className="navbar-collapse collapse">
+					<ul className="nav navbar-nav">
+						{pageLinks}
+					</ul>
+					{children}
+					<div>
+						<AccountMenu active={currentPage === 'account'} />
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+// ./NavBar
+
+
+// NB: the react-bootstrap version of this with Navbar, NavItem seems to have bugs in NavItem's handling of clicks :'(
+// ...yep, react-bootstrap's navbar has been broken for a year https://github.com/react-bootstrap/react-bootstrap/issues/2365
+// So we solve ourselves, with a custom on-click
+const navClick = (e) => {
+	// close the menu in mobile mode
+	let openMobileMenu = $('#navbar.collapse.in');
+	if (openMobileMenu.length) {
+		console.warn("better close it!")
+		$('button.navbar-toggle').click();
+	}
+};
+
+const NavLink = ({currentPage, targetPage, name}) => {
+	return (<li className={currentPage === targetPage? 'active' : ''}>
+				<a className="nav-item nav-link" href={'#'+targetPage} onClick={navClick} >
+					{name || targetPage}
+				</a>
+			</li>);
+};
+
+export default NavBar;
---
+++ base/data/Transfer.js
@@ -0,0 +1,53 @@
+
+import _ from 'lodash';
+import {assert, assMatch} from 'sjtest';
+import {isa, nonce, defineType} from './DataClass';
+import {uid, blockProp} from 'wwutils';
+import Money from './Money';
+import C from '../CBase';
+import Login from 'you-again';
+import DataStore from '../plumbing/DataStore';
+import ServerIO from '../plumbing/ServerIOBase';
+
+const Transfer = defineType(C.TYPES.Transfer);
+const This = Transfer;
+export default Transfer;
+
+
+/** Add up the prices of all the items in the basket 
+ * @returns {Money} never null
+*/
+Transfer.getTotal = (list, to) => {
+	assMatch(to, String);
+	// Using this clumsy forEach instead of a reduce because this makes it clearer
+	// that the total's Money object (thus currency) is based on the first item
+	let total = Money.make();
+	list.forEach((item) => {
+		This.assIsa(item);
+		let amount = item.amount;
+		Money.assIsa(amount);
+		if (item.to !== to) { // TODO user with multiple IDs, eg email+Twitter
+			// Login.iam(to)
+			amount = Money.mul(amount, -1);
+		}
+		total = Money.add(total, amount);
+	});
+	return total || Money.make();
+};
+
+/**
+ * TODO do this server-side
+ */
+Transfer.getCredit = (uxid) => {
+	if ( ! uxid) uxid = Login.getId();
+	if ( ! uxid) return null;
+	const pvCreditToMe = DataStore.fetch(['list', 'Transfer', 'toFrom:'+Login.getId()], () => {	
+		return ServerIO.load('/credit/_list', {data: {toFrom: Login.getId()} });
+	});
+	if (pvCreditToMe.value) {
+		// sum them
+		let cred = Transfer.getTotal(pvCreditToMe.value.hits, uxid);
+		return cred;
+	}
+	return null;
+};
