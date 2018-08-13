9869f0113371c9edae759e3d7fd67bd1c515d48c
Daniel Winterstein <daniel@sodash.com>
2018-08-13 22:28:23 +0100

    notes

--- base/components/BS.jsx
+++ base/components/BS.jsx
@@ -1,5 +1,11 @@
 /**
  * Allow for Bootstrap 3 or Bootstrap 4 -- depending on what MainDiv imports
+ * 
+ * To initialise this, add an import to MainDiv:
+ * import BS from '../base/components/BS3'; // or BS4
+ * This will populate BS with actual code.
+ * 
+ * BS3.jsx and BS4.jsx have the same interface, so our code can work across Bootstrap versions.
  */
 const BS = {};
 export default BS;
