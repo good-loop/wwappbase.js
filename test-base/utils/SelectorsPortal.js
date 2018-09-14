a2d1495ce4f9f353c376b6b3c0faca2fda89a486
Mark Wood <markw13a@gmail.com>
2018-09-14 11:32:26 +0100

    Update advert selectors

--- test-base/utils/SelectorsPortal.js
+++ test-base/utils/SelectorsPortal.js
@@ -24,21 +24,21 @@ const Advertiser = {
     'charity-url': "#advertiser > div > div > div.form > div.Card.panel.panel-warning > div.panel-body > div.container-fluid > div > div:nth-child(1) > div > div:nth-child(4) > div > input",
     'charity-white-logo': "#advertiser > div > div > div.form > div.Card.panel.panel-warning > div.panel-body > div.container-fluid > div > div:nth-child(1) > div > div:nth-child(5) > div > input",
     'charity-colour-logo': "#advertiser > div > div > div.form > div.Card.panel.panel-warning > div.panel-body > div.container-fluid > div > div:nth-child(1) > div > div:nth-child(7) > div > input",
-    'charity-small-print': "#advertiser > div > div > div.form > div.Card.panel.panel-warning > div.panel-body > div.container-fluid > div > div:nth-child(1) > div > div:nth-child(8) > input",
-    'charity-circle-crop-factor': "#advertiser > div > div > div.form > div.Card.panel.panel-warning > div.panel-body > div.container-fluid > div > div:nth-child(1) > div > div:nth-child(9) > input",
+    'charity-small-print': "#advertiser > div > div > div.form > div.Card.panel.panel-warning > div.panel-body > div.container-fluid > div > div:nth-child(1) > div > div:nth-child(9) > input",
+    'charity-circle-crop-factor': "#advertiser > div > div > div.form > div.Card.panel.panel-warning > div.panel-body > div.container-fluid > div > div:nth-child(1) > div > div:nth-child(10) > input",
     'parent-charity-id': "#advertiser > div > div > div.form > div.Card.panel.panel-warning > div.panel-body > div:nth-child(5) > div.form-group > div > input"
 };
 
 const Advert = {
     Create: '#advert > div > div:nth-child(2) > button',
     on: '#advert > div > div:nth-child(2) > div > div:nth-child(4) > div > label',
-    name: '#advert > div > div:nth-child(2) > div > div:nth-child(7) > input',
-    campaign: '#advert > div > div:nth-child(2) > div > div:nth-child(8) > input',
-    logo: '#advert > div > div:nth-child(2) > div > div.CardAccordion > div:nth-child(1) > div.panel-body > div:nth-child(1) > div > input',
-    'white-logo': '#advert > div > div:nth-child(2) > div > div.CardAccordion > div:nth-child(1) > div.panel-body > div:nth-child(2) > div > input',
-    about: '#advert > div > div:nth-child(2) > div > div.CardAccordion > div:nth-child(1) > div.panel-body > div:nth-child(6) > textarea',
-    video: '#advert > div > div:nth-child(2) > div > div.CardAccordion > div:nth-child(5) > div.panel-body > div:nth-child(1) > input',
-    'mobile-video': '#advert > div > div:nth-child(2) > div > div.CardAccordion > div:nth-child(5) > div.panel-body > div:nth-child(4) > input',
+    name: '#advert > div > div:nth-child(2) > div > div > div:nth-child(7) > input',
+    campaign: '#advert > div > div:nth-child(2) > div > div > div:nth-child(8) > input',
+    logo: '#advert > div > div:nth-child(2) > div > div > div.CardAccordion > div:nth-child(1) > div.panel-body > div:nth-child(1) > div > input',
+    'white-logo': '#advert > div > div:nth-child(2) > div > div > div.CardAccordion > div:nth-child(1) > div.panel-body > div:nth-child(2) > div > input',
+    about: '#advert > div > div:nth-child(2) > div > div > div.CardAccordion > div:nth-child(1) > div.panel-body > div:nth-child(6) > textarea',
+    video: '#advert > div > div:nth-child(2) > div > div > div.CardAccordion > div:nth-child(5) > div.panel-body > div:nth-child(1) > input',
+    'mobile-video': '#advert > div > div:nth-child(2) > div > div > div.CardAccordion > div:nth-child(5) > div.panel-body > div:nth-child(4) > input',
 };
 
 const Selectors = {
