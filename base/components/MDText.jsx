85463e49fac8403495bd701ca25caa72c059da57
Daniel Winterstein <daniel@winterwell.com>
2018-10-02 11:20:20 +0100

    testing-unicode

--- base/components/MDText.jsx
+++ base/components/MDText.jsx
@@ -4,6 +4,8 @@ import Misc from './Misc';
 
 /**
  * Remove non-standard characters and render Markdown.
+ * @param source {!String} text to render
+ * @param renderers ?? see react-markdown docs
  */
 const MDText = ({source, renderers}) => {
 	let nsource = Misc.normalise(source);
