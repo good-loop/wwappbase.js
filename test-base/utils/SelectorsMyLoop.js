aef26242904c14cba687069d6898376c546bc24c
Mark Wood <markw13a@gmail.com>
2018-10-26 10:07:11 +0100

    Updated my-loop selectors

--- test-base/utils/SelectorsMyLoop.js
+++ test-base/utils/SelectorsMyLoop.js
@@ -1,12 +1,12 @@
 const MyLoopSelectors = {
-    sign_up:"#my > div > div.container.avoid-navbar > div > div:nth-child(1) > div > div > div > a",
-    logged_in_greeting:"#my > div > div.container.avoid-navbar > div > div:nth-child(1) > div > div > div > div.pull-right.logged-in",
-    "personaliseAds":`#my > div > div.container.avoid-navbar > div > div:nth-child(8) > div.panel-body > div > div:nth-child(3) > div > label:nth-child(1)`,
+    sign_up:"a[href='https://testmy.good-loop.com/#my']",
+    logged_in_greeting:"div.WelcomeCard div.logged-in",
+    "personaliseAds":`#my > div > div.container.avoid-navbar > div > div:nth-child(6) > div.panel-body > div > div:nth-child(3) > div > label:nth-child(1)`,
     "-personaliseAds":"#my > div > div.container.avoid-navbar > div > div:nth-child(8) > div.panel-body > div > div:nth-child(3) > div > label:nth-child(2)",
     "-recordDonations":`#my > div > div.container.avoid-navbar > div > div:nth-child(8) > div.panel-body > div > div:nth-child(4) > div > label:nth-child(2) > input[type="radio"]`,
     "recordDonations":`#my > div > div.container.avoid-navbar > div > div:nth-child(8) > div.panel-body > div > div:nth-child(4) > div > label:nth-child(1) > input[type="radio"]`,
-    "-recordAdsBehaviour":`#my > div > div.container.avoid-navbar > div > div:nth-child(8) > div.panel-body > div > div:nth-child(5) > div > label:nth-child(2) > input[type="radio"]`,
-    "recordAdsBehaviour":`#my > div > div.container.avoid-navbar > div > div:nth-child(8) > div.panel-body > div > div:nth-child(5) > div > label:nth-child(1) > input[type="radio"]`
+    "-recordAdsBehaviour":`#my > div > div.container.avoid-navbar > div > div:nth-child(6) > div.panel-body > div > div:nth-child(6) > div > label:nth-child(2)`,
+    "recordAdsBehaviour":`#my > div > div.container.avoid-navbar > div > div:nth-child(6) > div.panel-body > div > div:nth-child(6) > div > label:nth-child(1)`
 };
 
 module.exports = MyLoopSelectors;
