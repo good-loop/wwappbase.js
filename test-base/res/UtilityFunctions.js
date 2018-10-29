694db9acb232c77bd12c70c039d3ff96d2fc878d
Mark Wood <markw13a@gmail.com>
2018-10-29 17:30:00 +0000

    Updated base login method to support Twitter and Facebook

--- test-base/res/UtilityFunctions.js
+++ test-base/res/UtilityFunctions.js
@@ -68,23 +68,96 @@ function timeout(ms) {
 
 /**Login to app. Should work for both SoGive and Good-loop 
  * Make sure that you are actually on a page with a clickable login button before running this!
+ * @param selectors CSS selectors for the given page
+ * @param url option param. Will go to the url before attempting to log in
+ * @param service how are you loggin in? Can be email, Twitter or Facebook
 */
-async function login({page, username, password}) {
+async function login({browser, page, username, password, Selectors, service}) {
     if(!username || !password) throw new Error('UtilityFunctions -- no username/password provided to login');
 
-    await page.addScriptTag(disableAnimations);
-    await page.waitForSelector(CommonSelectors['log-in']);
-    await page.click(CommonSelectors['log-in']);
-    await page.waitForSelector(CommonSelectors['log-in-email']);
-    await page.waitForSelector(CommonSelectors['log-in-password']);
+    // support for older tests that did not have these params
+    if( !Selectors ) Selectors = CommonSelectors;
+    if( !service ) service = 'email';
 
-    await page.click(CommonSelectors['log-in-email']);
-    await page.keyboard.type(username);  
-    await page.click(CommonSelectors['log-in-password']);
-    await page.keyboard.type(password); 
-    await page.keyboard.press('Enter');
+    await page.waitForSelector(Selectors['log-in']);
+    await page.click(Selectors['log-in']);
+
+    if( service === 'email' ) {
+        await page.waitForSelector(Selectors['log-in-email']);
+        await page.waitForSelector(Selectors['log-in-password']);
     
-    await page.waitForSelector(CommonSelectors['log-in-email'], {hidden: true});
+        await page.click(Selectors['log-in-email']);
+        await page.keyboard.type(username);  
+        await page.click(Selectors['log-in-password']);
+        await page.keyboard.type(password); 
+        await page.keyboard.press('Enter');
+        
+        await page.waitForSelector(Selectors['log-in-email'], {hidden: true});
+    }
+
+    if( service === 'twitter' ) {
+        await page.waitForSelector(Selectors.twitterLogin);
+        await page.click(Selectors.twitterLogin);
+        await page.waitForSelector(Selectors.apiUsername);
+    
+        await page.click(Selectors.apiUsername);
+        await page.keyboard.type(username);
+        await page.click(Selectors.apiPassword);
+        await page.keyboard.type(password);
+    
+        await page.click(Selectors.apiLogin);
+        // twitter, for some reason, wants you
+        // to enter the exact same username & password
+        // again, but on a different page
+        await page.waitForNavigation({ waitUntil: 'load'});
+        await page.waitFor(5000); // Give Twitter login a second to process
+        // await page.click(TwitterSelectors.username);
+        // await page.keyboard.type(twitterUsername);
+        // await page.click(TwitterSelectors.password);
+        // await page.keyboard.type(twitterPassword);
+        // await page.click(TwitterSelectors.login);
+    }
+
+    if( service === 'facebook' ) {
+
+        if( !browser ) throw new Error('login function needs to be passed a browser object when logging in via Facebook');
+
+        // return promise and await below
+        // workaround for issue where Jest would reach end of test
+        // and deem it a success without waiting for the browser.on
+        // callback to finish executing
+        let fbResolve;
+        let fbLoginFinished = new Promise(function(resolve, reject) { fbResolve = resolve; });
+        fbLoginFinished.resolve = fbResolve;
+
+        browser.on('targetcreated', async(target) => {
+            if(target._targetInfo.type !== 'page') return;
+            const fbPage = await target.page();
+
+            await fbPage.waitForSelector(Selectors.username);
+            await fbPage.click(Selectors.username);
+            await fbPage.keyboard.type(username);
+
+            await fbPage.click(Selectors.password);
+            await fbPage.keyboard.type(password);
+            await fbPage.click(Selectors.login);
+
+            // only seems to appear once...
+            // await fbPage.waitForSelector(FacebookSelectors.continue);
+            // await fbPage.click(FacebookSelectors.continue);
+
+            fbLoginFinished.resolve();
+        });
+
+        // trigger above code to handle
+        // facebook login page
+        // second click to handle popup being blocked
+        await page.click(Selectors.facebookLogin);
+        await page.click(Selectors.facebookLogin);
+
+        // check that user is logged in, fail test if not
+        await fbLoginFinished;
+    }
 }
 
 /**
@@ -96,13 +169,17 @@ async function fillInForm({page, Selectors, data}) {
     for(let i=0; i<keys.length; i++){
         const key = keys[i];
         const selector = Selectors[key];
-        //Only clicks checkbox if value doesn't match boolean provided. Would be easier to directly manipulate the DOM state,
-        //but that doesn't seems to defeat the purpose of end-to-end testing
-        if(await page.$eval(selector, e => e.type) === 'checkbox') {
+
+        //Clicks checkbox if value doesn't match boolean provided
+        if( await page.$eval(selector, e => e.type) === 'checkbox' ) {
                 //Would be nicer to have this as one if statement, but there is a bit of faff around passing arguments into page.$eval()
                 const checkValue = await page.$eval(selector, e => e.checked);
-                if(checkValue != data[key]) await page.click(selector)
-            }
+                if( checkValue != data[key] ) await page.click(selector)
+        }
+        // Select drop-down menu option
+        else if( await page.$eval(selector, e => e.tagName) === 'SELECT' ) {
+            await page.select(selector, data[key]);
+        }
         else {
             await page.click(selector);
             //Check for default value. Clear field if found
