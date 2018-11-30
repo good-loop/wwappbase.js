c097d83761924eb8bbf78a996fcba929df559ea8
DW-Stross <daniel@good-loop.com>
2018-11-30 16:24:50 +0000

    Share

---
+++ base/data/Share.js
@@ -0,0 +1,27 @@
+/**
+ * You-Again Share object
+ * ??move this into you-again.js??
+ */
+
+import {assert, assMatch} from 'sjtest';
+import {isa, defineType, getType} from './DataClass';
+
+const Share = defineType('Share');
+const This = Share;
+export default This;
+
+/**
+ * Support Share or DBShare (c.f. the java server-side code)
+ */
+This.isa = (obj) => isa(obj, 'Share') || getType(obj) === 'DBShare';
+
+/**
+ * Convenience for a common filter + strip op.
+ * @param {!Share[]} shareList
+ * @param {!String} prefix Note: this should usually end with a ":", e.g. "Publisher:"
+ * @returns {String[]} sharelist filtered by prefix, with item extracted and the prefix removed.
+ */
+Share.stripPrefix = (shareList, prefix) => {
+    let slist2 = shareList.filter(s => This.assIsa(s) && s.item.startsWith(prefix));
+    return slist2.map(s => s.item.substr(prefix.length));
+};
