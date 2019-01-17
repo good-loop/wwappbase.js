78dd57bae45b70192af67ddfca942f8d613ac318
Mark Wood <markw13a@gmail.com>
2019-01-17 12:46:22 +0000

    WIP updating tests to work with preact

--- test-base/utils/SelectorsAdServer.js
+++ test-base/utils/SelectorsAdServer.js
@@ -1,6 +1,6 @@
 const TestAs = {
     FirstCharityIcon: "div.charity-chooser:not(.not-ready) > ul > li:nth-child(1)",
-    Banner: "div.glhdr",
+    Banner: "#banner",
     ClickToPlay: ".gl-play-video:not(.gl-hidden)"
 };
 
