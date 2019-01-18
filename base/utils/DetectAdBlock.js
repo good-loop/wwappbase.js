2d5ca1b48f42a7f805f5e89f1eda7ba25ec8587c
Mark Wood <markw13a@gmail.com>
2019-01-18 15:49:07 +0000

    Generic adblock detector

---
+++ base/utils/DetectAdBlock.js
@@ -0,0 +1,23 @@
+import DataStore from '../plumbing/DataStore';
+
+/** Will set DataStore flag if the user has adblock enabled */
+const DetectAdBlock = ({path}) => {
+    const $script = document.createElement('script');
+    // Based on https://www.detectadblock.com/
+    // Adblockers are expected to always block js files with "ads" in the name
+    $script.setAttribute('src', 'https://as.good-loop.com/ads.js');
+    
+    $script.onload = () => {
+        // If adblocker enabled, ads.js will not be able to create div with id #aiPai9th 
+        const adBlockEnabled = ! document.getElementById('aiPai9th');
+        DataStore.setValue(path, adBlockEnabled);
+    };
+
+    $script.onerror = () => {
+        DataStore.setValue(path, true);
+    };
+
+    document.head.appendChild($script);
+};
+
+module.exports = DetectAdBlock;
