e8ec5e18c7fd90ad9d2f36f5a8b540707cf6e8ad
DW-Stross <daniel@good-loop.com>
2019-01-25 13:51:47 +0000

    link extends claim

--- base/data/Person.js
+++ base/data/Person.js
@@ -16,11 +16,28 @@ Person.linkedIds = peep => peep.links? peep.links.map(link => link.to) : [];
 Person.getTwitterXId = () => getSocialXId('twitter')
 Person.getFacebookXId = () => getSocialXId('facebook');
 
-// Function generator
-// Trawls through xids in Datastore and returns one that matches given service
-
+/** @deprecated Dubious - could return another persons data from DataStore
+ * 
+ * Trawls through xids in Datastore and returns one that matches given service */
 const getSocialXId = (service) => {
     const xids = Object.keys(DataStore.getValue(['data', 'Person']));
 
     return xids.find( xid =>  xid.match(/@(.*)/)[1] === service );
+};
+
+
+/**
+ * 
+ * @param {Person} peep 
+ * @param {String} service
+ * @returns {?Link} The (TODO most likely) email link
+ */
+Person.getLink = (peep, service) => {
+	Person.assIsa(peep);
+	// is the XId an email XId?
+	const xid = Person.getId(peep);
+	if (XId.service(xid) === 'email')
+
+	// Test claims too? No - lets enforce clean data for ourselves
+	return null;
 };
