e8201faccb7bbac9c049742bcc0a3b65abb060dd
Mark Wood <markw13a@gmail.com>
2019-01-28 11:21:10 +0000

    Updated selectors

---
+++ test-base/utils/SelectorsDemoPage.js
@@ -0,0 +1,13 @@
+module.exports = {
+    // Format selector + options
+    'select-element': '#controlformat-desk',
+    'mpu2': 'option[value="mpu2"]',
+    'pre-roll': 'option[value="pre-roll"]',
+    'in-read': 'option[value="in-read"]',
+    'mpu': 'option[value="mpu"]',
+    // Form fields
+    'name': 'input[name="namefield"]',
+    'email': 'input[name="email"]',
+    'message': 'textarea[name="message"]',
+    'submit': 'button[type="submit"]'
+};
