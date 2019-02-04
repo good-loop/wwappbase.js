a4dd3da386e464eed9071cce7eab3eca669ca9dc
Mark Wood <markw13a@gmail.com>
2019-02-04 17:26:13 +0000

    Added selectors for new pagewrapper test

--- test-base/utils/SelectorsAdServer.js
+++ test-base/utils/SelectorsAdServer.js
@@ -1,7 +1,10 @@
 const TestAs = {
     FirstCharityIcon: "ul.chooser-list.ready > a:nth-child(1)",
     Banner: "#banner",
-    ClickToPlay: ".gl-play-video"
+    ClickToPlay: ".gl-play-video",
+    TwitterLink: "a[alt='Twitter']",
+    LinkedInLink: "a[alt='LinkedIn']",
+    FacebookLink: "a[alt='Facebook']"
 };
 
 module.exports = {
