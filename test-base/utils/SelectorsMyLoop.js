07eb0b97e5ac0b72c9dab3dcb7b704e711b71b84
Mark Wood <markw13a@gmail.com>
2019-02-28 13:03:08 +0000

    Fix out-dated selectors

--- test-base/utils/SelectorsMyLoop.js
+++ test-base/utils/SelectorsMyLoop.js
@@ -1,5 +1,5 @@
 const MyLoopSelectors = {
-    'log-in':"#my > div > div.container.avoid-navbar > div > div.Card.panel.panel-default.headerCard > div > div > div > div.col-md-7.header-text.frank-font > a",
+    'log-in':"#my > div > div.container.avoid-navbar > div > div.Card.panel.panel-default.headerCard > div > div > div > div.col-md-7.header-text.frank-font > div > a",
     logged_in_greeting:"div.WelcomeCard div.logged-in",
     name: `#my > div > div.container.avoid-navbar > div > div:nth-child(4) > div.panel-body > div:nth-child(1) > div > div.mirror > div.container.word-wrap > div > div.col-sm-5.col-sm-pull-5.profile-details > div:nth-child(1) > div.col-md-8 > input`,
     gender: `#my > div > div.container.avoid-navbar > div > div:nth-child(4) > div.panel-body > div:nth-child(1) > div > div.mirror > div.container.word-wrap > div > div.col-sm-5.col-sm-pull-5.profile-details > div:nth-child(2) > div.col-md-8 > select`,
@@ -12,7 +12,7 @@ const MyLoopSelectors = {
     "sendMessages":`div.form-group label.radio-inline input[name="sendMessages"][value="true"]`,
     "-sendMessages":`div.form-group label.radio-inline input[name="sendMessages"][value="false"]`,
     "personaliseAds":`div.form-group label.radio-inline input[name="personaliseAds"][value="true"]`,
-    "-personaliseAds":`div.form-group label.radio-inline input[name="personaliseAds"][value="false"]`,
+    "-personaliseAds":`div.form-group label.radio-inline input[name="personaliseAds"][value="false"]`
 };
 
 module.exports = MyLoopSelectors;
