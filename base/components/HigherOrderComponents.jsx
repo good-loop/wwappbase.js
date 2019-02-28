4a937361aa847e8173765a877af4dbe48ba1103e
Mark Wood <markw13a@gmail.com>
2019-02-28 17:33:56 +0000

    Turns out not all components are DigitalMirror. Who'd have thought

--- base/components/HigherOrderComponents.jsx
+++ base/components/HigherOrderComponents.jsx
@@ -3,6 +3,7 @@ import ServerIO from '../plumbing/ServerIOBase';
 // https://reactjs.org/docs/higher-order-components.html
 // Reusable bits of functionality: simply wrap your component with one of these to extend its functionality
 
+/** Logging will not work properly with anonymous functions (they do not have a displayName or name property) */
 const withLogsIfVisible = (Component) => (props) => {
 	// Report if this div appeared fully on the user's screen
 	let logsIfVisibleRef = useRef();
@@ -10,7 +11,7 @@ const withLogsIfVisible = (Component) => (props) => {
 		const scrollListener = window.addEventListener(
 			'scroll',
 			// Pass in reference to actual DOM element 
-			() => ServerIO.logIfVisible(logsIfVisibleRef.current, "DigitalMirrorVisible")
+			() => ServerIO.logIfVisible(logsIfVisibleRef.current, ( Component.dispayName || Component.name || 'UnknownComponent') + 'Visible')
 		);
 		return () => window.removeEventListener('scroll', scrollListener);
 	}, [logsIfVisibleRef]);
