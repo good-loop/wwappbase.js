9265fcfd67cbe847492d78e5e9f42f0043c50ad2
Mark Wood <markw13a@gmail.com>
2019-03-11 11:51:30 +0000

    Working, but with bugs

--- base/components/GoodLoopUnit.jsx
+++ base/components/GoodLoopUnit.jsx
@@ -1,141 +1,81 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import ServerIO from '../plumbing/ServerIOBase';
 import { assert } from 'sjtest';
 
-const injectGoodLoopUnit = ({ adID, CSS, adunitRef, iframe, size }) => {
-    // Important that this is not falsy
-    if( !size ) size = 'landscape';
-
-    // GoodLoop unit script
-    const $script = document.createElement('script');
-    $script.async = true;
-    let src = ServerIO.AS_ENDPOINT + '/unit.js?gl.size=' + size + '&';
-    if ( adID ) src += ('gl.vert=' + adID + '&');
-    $script.setAttribute('src', src);
-
-    // Prevent adunit from wrapping itself in its own (cross-domain) iframe
-    const $container = document.createElement('div');
-    $container.id = "unit-container"
-    const $noFrameMarker = document.createElement('div');
-    $noFrameMarker.id = "glNoFrameMarker";
-    
-    const $link = document.createElement('link');
-    $link.type = 'text/css';
-    $link.rel = 'stylesheet';
-    $link.href =  ServerIO.AS_ENDPOINT + '/unit.css';
-
-    iframe.setAttribute('id', 'good-loop-iframe');
-    iframe.setAttribute('frameborder', 0);
-    iframe.setAttribute('scrolling', 'auto');
-    
-    // Set appopriate height and width for gl.size selected
-    if ( size === 'landscape') {
-        iframe.style.height = '360px';
-        iframe.style.width = '640px';
-    } else if ( size === 'portrait' ) {
-        iframe.style.height = '800px';
-        iframe.style.width = '450px';
-    }
-
-    iframe.addEventListener('load', () => {
-        window.iframe = iframe;
-        iframe.contentDocument.body.style.overflow = 'hidden';
-        iframe.contentDocument.body.appendChild($script);
-        iframe.contentDocument.body.appendChild($container);
-        iframe.contentDocument.body.appendChild($noFrameMarker);
-        iframe.contentDocument.head.appendChild($link);
-    });
-
-    $script.addEventListener('load', () => {
-        // Insert CSS provided by prop in to unit
-        insertAdunitCSS({ iframe, CSS })
-    });
-
-    adunitRef.appendChild(iframe);
-};
-
-// For custom CSS. Used by MockUpGenerator to preview changes on the fly.
-const insertAdunitCSS = ({ iframe, CSS }) => {
-    if( !CSS ) return;
-    assMatch(CSS, 'String');
-
+const insertAdunitCSS = ({iframe, CSS}) => {
     const $style = document.createElement('style');
     $style.type = 'text/css';
     $style.id = 'vert-css';
     $style.innerHTML = CSS;
 
-    // Has the adunit already inserted a custom CSS tag?
-    // If so, delete it. Use querySelectorAll in case multiple tags were accidentaly inserted
-    const $adunitCSS = iframe.contentDocument.querySelectorAll('#vert-css');
-    if( $adunitCSS ) {
-        $adunitCSS.forEach( node => node.parentElement.removeChild(node) );
-    } 
+    console.warn(iframe);
     // (18/02/19) Inserting in to body instead of head is a dumb dumb fix for adunit inserting it's style tag after insertAdunitCSS has already run
     // Means that, if a user makes edits and then reloads the page, they will see the published ad's CSS rather than their local changes.
     // Don't think that there is any event I can listen for, and I did not want to have this function run in the render method.
     iframe.contentDocument.body.appendChild($style);
 };
 
-class GoodLoopUnit extends React.Component {
-    constructor(props) {
-        super(props);
+const GoodLoopUnit = ({ adID, CSS, size }) => {
+    // TODO: investigate this
+    // Looked as though default parameter was ignored if falsy value is provided as argument to GoodLoopUnit
+    size = size || 'landscape';
 
-        this.adunitRef = null;
+    // Get reference to iframe div instantiated by React
+    let iframeRef = useRef();
+    useEffect( () => iframeRef.current.focus() );
 
-		this.setRef = (ref, value) => {
-			this[ref] = value;
-		};
+    // Load/Reload the adunit
+    useEffect( () => {
+        const iframe = iframeRef.current;
+        if( !iframe ) return;
 
-		this.focusRef = (ref) => {
-			if (this[ref]) this[ref].focus();
-        };
-        
-        // Reference to iframe containing GoodLoop unit
-        const iframe = document.createElement('iframe');
+        // Load specific ad if given, random ad if not
+        let src = ServerIO.AS_ENDPOINT + '/unit.js?gl.status=DRAFT&gl.size=' + size + '&';
+        if ( adID ) src += ('gl.vert=' + adID + '&');
 
-        this.state = { iframe };
-    }
-    
-    componentDidMount() {
-        this.focusRef('adunitRef');
-        const { adunitRef } = this;
-        const { iframe } = this.state;
+        const $container = document.createElement('div');
+        $container.className = 'goodloopad'; 
 
-        injectGoodLoopUnit({ ...this.props, adunitRef, iframe });
-    }
+        const $script = document.createElement('script');
+        $script.src = src;
+        $script.async = true;
 
-    componentWillUpdate(nextProps, nextState) {
-        const { CSS: nextCSS, size: nextSize, adID: nextadID } = nextProps;
-        const { CSS, size, adID } = this.props;
-        const { iframe } = this.state;
-        this.props = nextProps;
-        this.state = nextState;
+        iframe.contentDocument.body.appendChild($script);
+        iframe.contentDocument.body.appendChild($container);
 
-        // Reload the adunit
-        if ( nextSize !== size || nextadID !== adID ) {
-            const { adunitRef } = this;
+        // return () => iframe.contentDocument && iframe.contentDocument.body ? iframe.contentDocument.body = '' : null;
+    }, [adID, size, iframeRef]);
 
-            // Delete current iframe from DOM
-            iframe.parentElement.removeChild(iframe);
-            
-            // Create new iframe, fill with adunit, then insert
-            const nextIframe = document.createElement('iframe');
-            injectGoodLoopUnit( {...nextProps, iframe: nextIframe, adunitRef } );
-            // Update iframe reference in state
-            this.setState({ iframe: nextIframe });
-            return;
+    // Insert CSS in to the head
+    // Decided to continue with this rather than loading DRAFT because I have no good way of knowing when back-end will have updated with user's changes. At best will be much slower.
+    let goodloopframe = iframeRef.current && iframeRef.current.contentDocument && iframeRef.current.contentDocument.querySelector('.goodloopframe');
+    useEffect( () => {
+        if( !goodloopframe ) return;
+
+        insertAdunitCSS({iframe: goodloopframe, CSS});
+        // Has the adunit already inserted a custom CSS tag?
+        // If so, delete it. Use querySelectorAll in case multiple tags were accidentaly inserted
+        return () => {
+            const $adunitCSS = goodloopframe.querySelectorAll('#vert-css') || [];
+            console.warn("Why are these not removed?", $adunitCSS);
+            $adunitCSS.forEach( node => node.parentElement.removeChild(node) );
         }
+    }, [CSS, goodloopframe]);
 
-        // No need to reload, just replace CSS in head
-        if( nextCSS !== CSS ) {
-            insertAdunitCSS({ iframe, CSS: nextCSS });
-            return;
-        }
-    }
+    let frameStyle = {};
+    if ( size === 'landscape') {
+        frameStyle.height = '360px';
+        frameStyle.width = '640px';
+    } else if ( size === 'portrait' ) {
+        frameStyle.height = '800px';
+        frameStyle.width = '450px';
+    }   
 
-    render() {
-        return <div ref={e => this.setRef('adunitRef', e)} />;
-    }
-}
+    return (
+        <div className="goodLoopContainer">
+            <iframe ref={iframeRef} frameBorder={0} scrolling='auto' style={frameStyle} />                
+        </div>
+    );
+};
 
 module.exports = GoodLoopUnit;
