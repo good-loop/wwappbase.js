aaf670f32b0a1048486da390aef4140f0de56f38
DA <daniel.appel.winterwell@gmail.com>
2019-03-13 17:27:39 +0000

    added 'web-apps' directory to the list of directories to sync during an adserver publish

--- project-publisher.sh
+++ project-publisher.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-VERSION='Version=1.16.1'
+VERSION='Version=1.16.2'
 
 ###
+# New in 1.16.2: added 'web-apps' directory to the list of directories to sync during an adserver publish
 # New in 1.16.1: Enabled the sogive-app to update the version.properties file
 # New in 1.16.0: Moneyscript is now a publishable project
 # New in 1.15.7: changed the portal npm command to 'npm run compile'
@@ -193,7 +194,7 @@ case $1 in
 		UNITS_LOCATION="$PROJECT_LOCATION/adunit/variants/"
 		RESTART_SERVICE_AFTER_SYNC='yes'
 		SERVICE_NAME=('adservermain')
-		FRONTEND_SYNC_LIST=("adunit" "server" "src" "web-as" "web-test" "preact-unit" "package.json" "webpack.config.as.js" "webpack.config.js" ".babelrc")
+		FRONTEND_SYNC_LIST=("adunit" "server" "src" "web-apps" "web-as" "web-test" "preact-unit" "package.json" "webpack.config.as.js" "webpack.config.js" ".babelrc")
 		BACKEND_SYNC_LIST=("lib")
 		WHOLE_SYNC=("adunit" "server" "src" "web-as" "web-test" "preact-unit" "package.json" "webpack.config.as.js" "webpack.config.js" ".babelrc" "lib")
 		PRESERVE=("config/log.properties")
