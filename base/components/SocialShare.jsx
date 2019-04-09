ff5b21a69f070d72a5bba140bb9a113d52f0b18b
Mark Wood <markw13a@gmail.com>
2019-04-09 16:27:56 +0100

    Fixed some url paramaters being chopped off by Twitter

--- base/components/SocialShare.jsx
+++ base/components/SocialShare.jsx
@@ -20,24 +20,26 @@ import NGO from '../data/NGO';
 const IntentLink = ({children, service, style, text, url}) => {
 	service = service.toLowerCase();
 
+	url = encodeURIComponent(url);
+
 	let href;
 	let icon;
 	if ( service === 'twitter' ) {
-		href = `https://twitter.com/intent/tweet?text=${text}&tw_p=tweetbutton&url=${url}`;
+		href = `https://twitter.com/intent/tweet?text=${text}&amp;tw_p=tweetbutton&amp;url=${url}`;
 		icon = (
 			<div className='intent-link' style={{backgroundColor: '#1b95e0', ...style}}>
 				<img alt='Twitter Logo' src='/img/twitter.png' crop="50%" title='Twitter Logo' />
 			</div>
 		);
 	} else if ( service === 'facebook' ) {
-		href = `http://www.facebook.com/sharer.php?u=${url}&quote=${text}`;
+		href = `http://www.facebook.com/sharer.php?u=${url}&amp;quote=${text}`;
 		icon = (
 			<div className='intent-link' style={{backgroundColor: '#4267b2', ...style}}>
 				<img alt='Facebook Logo' src='/img/facebook.png' crop="50%" title='Facebook Logo' />
 			</div>
 		);
 	} else if ( service === 'linkedin' ) {
-		href = `https://www.linkedin.com/shareArticle?mini=true&title=Our%20ads%20are%20raising%20money%20for%20charity&url=${url}&summary=${text}`;
+		href = `https://www.linkedin.com/shareArticle?mini=true&amp;title=Our%20ads%20are%20raising%20money%20for%20charity&amp;url=${url}&amp;summary=${text}`;
 		icon = (
 			<div className='intent-link' style={{backgroundColor: '#0077B5', ...style}}>
 				<img alt='LinkedIn Logo' src='/img/linkedin-white.png' crop="50%" title='LinkedIn Logo' />
@@ -49,7 +51,7 @@ const IntentLink = ({children, service, style, text, url}) => {
 	}
 
 	return (
-		<a href={encodeURI(href)} target="_blank" rel="noreferrer" style={{color: '#fff', display: 'inline-block'}} >
+		<a href={href} target="_blank" rel="noreferrer" style={{color: '#fff', display: 'inline-block'}} >
 			{ children || icon }
 		</a>
 	);
