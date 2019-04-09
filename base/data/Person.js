08d4b5164e26b673e8f0b96d47bcf8947862c84b
Mark Wood <markw13a@gmail.com>
2019-04-09 13:17:08 +0100

    Deleted deprecated, and unused, code

--- base/data/Person.js
+++ base/data/Person.js
@@ -30,19 +30,6 @@ export default Person;
 // filter only adds value to return array if it is not falsy
 Person.linkedIds = peep => peep.links? peep.links.reduce(Link.to, []) : [];
 
-Person.getTwitterXId = () => getSocialXId('twitter')
-Person.getFacebookXId = () => getSocialXId('facebook');
-
-/** @deprecated Dubious - could return another persons data from DataStore
- * 
- * Trawls through xids in Datastore and returns one that matches given service */
-const getSocialXId = (service) => {
-	const xids = Object.keys(DataStore.getValue(['data', 'Person']));
-
-	return xids.find( xid =>  xid.match(/@(.*)/)[1] === service );
-};
-
-
 /**
  * 
  * @param {Person} peep 
