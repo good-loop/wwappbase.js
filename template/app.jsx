b3a9575b026fd527818f725c944bab1d9974b51b
Daniel Winterstein <daniel@sodash.com>
2019-07-14 18:41:38 +0100

    app

---
+++ template/app.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import SJTest, {assert} from 'sjtest';
+
+import MainDiv from './components/MainDiv';
+
+ReactDOM.render(
+	<MainDiv />,
+	document.getElementById('mainDiv')
+	);
