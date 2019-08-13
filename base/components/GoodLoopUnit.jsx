d7690e497958d56ec97477cdf00c5cf9e61b9aba
Roscoe <roscoe@winterwell.com>
2019-08-13 18:12:21 +0100

    GoodLoopUnit: Resolve race conditions by waiting for iframe onLoad before appending anything to doc!

--- base/components/GoodLoopUnit.jsx
+++ base/components/GoodLoopUnit.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useState, useCallback } from 'react';
 import ServerIO from '../plumbing/ServerIOBase';
 
 
@@ -9,6 +9,7 @@ const appendEl = (doc, {tag, ...attrs}) => {
 		el[key] = val;
 	});
 	doc.body.appendChild(el);
+	return el; // just in case we want it
 }
 
 
@@ -27,7 +28,10 @@ const insertAdunitCss = ({frame, css}) => {
 	appendEl(frame.contentDocument, {tag: 'style', type: 'text/css', id: 'vert-css', class: 'override', innerHTML: css});
 
 	// On unmount: remove the CSS override we inserted but not the original if somehow present
-	return () => removeAdunitCss({frame, selector: '#vert-css.override'});
+	return () => {
+		console.log('insertAdunitCss on-unmount called');
+		removeAdunitCss({frame, selector: '#vert-css.override'})
+	};
 };
 
 
@@ -42,6 +46,7 @@ const removeAdunitCss = ({frame, selector = '#vert-css'}) => {
 
 const insertUnit = ({frame, unitJson, vertId, status, size}) => {
 	if (!frame) return;
+	console.log('*** inserting unit', frame);
 	const doc = frame.contentDocument;
 	const docBody = doc && doc.body;
 
@@ -49,7 +54,7 @@ const insertUnit = ({frame, unitJson, vertId, status, size}) => {
 	if (unitJson) appendEl(doc, {tag: 'div', id: 'preloaded-unit-json', innerHTML: unitJson});
 
 	// Insert the element the unit goes in
-	appendEl(doc, {tag: 'div', className:'goodloopad'});
+	console.log('inserting div.goodloopad', docBody, appendEl(doc, {tag: 'div', className:'goodloopad'}));
 
 	// Insert unit.js
 	let params = []
@@ -57,25 +62,38 @@ const insertUnit = ({frame, unitJson, vertId, status, size}) => {
 	if (size) params.push(`gl.size=${size}`); // If size isn't specified, the unit will pick a player-type to fit the container
 	if (vertId) params.push(`gl.vert=${vertId}`); // If adID isn't specified, we'll get a random ad.
 	const src = `${ServerIO.AS_ENDPOINT}/unit.js${params.length ? '?' + params.join('&') : ''}`;
-	appendEl(doc, {tag: 'script', src, async: true});
+	console.log('inserting <script>', docBody, appendEl(doc, {tag: 'script', src, async: true}));
 
 	// On unmount: empty out iframe's document
-	return () => { docBody ? docBody.innerHTML = '' : null; };
+	return () => {
+		console.log('insertUnit on-unmount called');
+		docBody ? docBody.innerHTML = '' : null;
+	};
 };
 
 
+// Record the container and iframe when they're put in the DOM
+// Ref function defined OUTSIDE render so its identity is static
+const receiveRef = (name, node, state, setState) => {
+	console.log('GoodLoopUnit', 'receiving ref', name, node);
+	if (node && node !== state[name]) {
+		console.log('GoodLoopUnit', 'storing ref', name, node);
+		// Make element available in this function and provoke a redraw
+		setState({...state, [name]: node});
+	}
+}
+
 const GoodLoopUnit = ({vertId, css, size = 'landscape', status, unitJson}) => {
 	// Store refs to the .goodLoopContainer and iframe nodes, to calculate sizing & insert elements
-	const [state, setState] = useState({});
-	const {container, frame} = state;
+	const [frame, setFrame] = useState();
+	const [frameLoaded, setFrameLoaded] = useState(false);
+	const [container, setContainer] = useState();
+	const [dummy, redraw] = useState(); // Just use this to provoke a redraw
 
-	// Record the container and iframe when they're put in the DOM
-	const receiveRef = ((name, node) => {
-		if (node && node !== state[name]) {
-			// Make element available in this function and provoke a redraw
-			setState({...state, [name]: node});
-		}
-	});
+	console.log('**** rendering', frame, container);
+
+	const receiveFrame = useCallback(node => setFrame(node), []);
+	const receiveContainer = useCallback(node => setContainer(node), []);
 
 	const frameDoc = frame && frame.contentDocument;
 	const goodloopframe = frameDoc && frameDoc.querySelector('.goodloopframe');
@@ -84,16 +102,18 @@ const GoodLoopUnit = ({vertId, css, size = 'landscape', status, unitJson}) => {
 	useEffect(() => insertAdunitCss({frame: goodloopframe, css}), [css, goodloopframe]);
 
 	// Load/Reload the adunit when vert-ID, unit size, or iframe container changes
-	useEffect(() => insertUnit({frame, unitJson, vertId, status, size}), [frame, unitJson, vertId, size, status]);
+	useEffect(() => {
+		if (frameLoaded) insertUnit({frame, unitJson, vertId, status, size});
+	}, [frameLoaded, frame, unitJson, vertId, size, status]);
 
 	// Set up listeners to redraw this component on window resize or rotate
 	useEffect(() => {
-		window.addEventListener('resize', setState);
-		window.addEventListener('orientationchange', setState);
+		window.addEventListener('resize', redraw);
+		window.addEventListener('orientationchange', redraw);
 
 		return () => {
-			window.removeEventListener('resize', setState);
-			window.removeEventListener('orientationchange', setState);
+			window.removeEventListener('resize', redraw);
+			window.removeEventListener('orientationchange', redraw);
 		};
 	}, []);
 
@@ -107,8 +127,8 @@ const GoodLoopUnit = ({vertId, css, size = 'landscape', status, unitJson}) => {
 	}
 
 	return (
-		<div className="goodLoopContainer" style={dims} ref={(node) => receiveRef('container', node)}>
-			<iframe frameBorder={0} scrolling='auto' style={{width: '100%', height: '100%'}} ref={(node) => receiveRef('frame', node)} />
+		<div className="goodLoopContainer" style={dims} ref={receiveContainer}>
+			<iframe frameBorder={0} scrolling='auto' style={{width: '100%', height: '100%'}} onLoad={() => setFrameLoaded(true)} ref={receiveFrame} />
 		</div>
 	);
 };
