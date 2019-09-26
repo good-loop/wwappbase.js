2f1e21979ca3652fd42a9f445bd9f2448cf13ca6
Daniel Winterstein <daniel@sodash.com>
2019-09-26 23:03:27 +0100

    CSS

---
+++ base/components/CSS.jsx
@@ -0,0 +1,31 @@
+/**
+ * A css node for custom css
+ */
+import React, {useState, useRef} from 'react';
+
+
+// /**
+//  * Create a CSS node containing the literal string provided
+//  */
+// const CSS = ({css, nodeId, insertionPoint = document.head}) => {
+// 	if ( ! css) return null;
+// 	if (done[css]) return null;
+// 	const styleNode = document.createElement('style');
+// 	styleNode.type = 'text/css';
+// 	if (nodeId) styleNode.id = nodeId;
+// 	if (styleNode.styleSheet) { // IE8 support TODO Really just IE8? Why do we have this?
+// 		styleNode.styleSheet.cssText = css;
+// 	} else { // literally every other browser
+// 		styleNode.appendChild(document.createTextNode(css));
+// 	}
+// 	insertionPoint.appendChild(styleNode);
+
+// 	// Remember to clean up later, if the 
+// 	addUninstallAction({ action: () => (insertionPoint && insertionPoint.removeChild(styleNode)) });
+// };
+
+const CSS = ({css}) => {
+	return <style>{css}</style>;
+};
+
+export default CSS;
