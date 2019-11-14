e0d459e4abbd8f17f87927b07fb00ba380f79f81
DW-Stross <daniel@good-loop.com>
2019-11-14 17:32:05 +0000

    sync

--- base/data/NGO.js
+++ base/data/NGO.js
@@ -13,3 +13,28 @@ DataClass.register(NGO, "NGO");
 export default NGO;
 
 NGO.description = (ngo) => ngo.description;
+
+/**
+ * UN goals -- 1 indexed! 
+ * (0 is a dummy - use this for things that don't fit)
+ * See 
+ */
+NGO.UNSDGs = [
+	"no clear fit", // no goal 0
+	"No Poverty",
+	"Zero Hunger",
+	"Good Health and Well-being",
+	"Quality Education",
+	"Gender Equality",
+	"Clean Water and Sanitation",
+	"Affordable and Clean Energy",
+	"Decent Work and Economic Growth",
+	"Industry, Innovation, and Infrastructure",
+	"Reducing Inequality",
+	"Sustainable Cities and Communities",
+	"Responsible Consumption and Production",
+	"Climate Action",
+	"Life Below Water",
+	"Life On Land",
+	"Peace, Justice, and Strong Institutions",
+	"Partnerships for the Goals"];
