bf5ed9d22115b6513aedf1b3550a6770bbe35d26
Roscoe <roscoe@winterwell.com>
2020-05-01 18:54:59 +0100

    CardAccordion working again + using internal state hooks

--- base/components/CardAccordion.jsx
+++ base/components/CardAccordion.jsx
@@ -1,5 +1,5 @@
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Card as BSCard, CardHeader, CardBody, Button } from 'reactstrap';
 import Misc from './Misc';
 import DataStore from '../plumbing/DataStore';
@@ -15,10 +15,6 @@ import { join } from './BS';
  * Note: If you see a card missing collapse controls -- this is probably the issue.
  *
  * @param {String|JSX} title - will be wrapper in h3 If this is null and titleChildren are null -- then there is no card header.
- *
- * TODO What is the use-case for titleChildren, titleClassName?? Deprecated??
- *
- * @param titleChildren jsx elements to put in the header (can be used with/without title)
  * @param {any} error - If set, colour the card red
  * @param warning {any} If set, colour the card yellow
  * @param {?String} className - Added to the BS panel classes
@@ -39,18 +35,17 @@ class Card extends React.Component {
 		// ??HACK expose this card to its innards via a global
 		// Card.current = this;
 
-		let {title, glyph, icon, children, className, onHeaderClick, collapse, titleChildren, titleClassName, warning, error, ...props} = this.props;
+		let { title, glyph, icon, children, className, onHeaderClick, collapse, warning, error } = this.props;
 		// no body = no card. Use case: so card guts (where the business logic often is) can choose to hide the card.
 		// Note: null should be returned from the top-level. If the null is returned from a nested tag, it may not be null yet, leading to the card showing.
-		if (!children) { return null; }
+		if (!children) return null;
 
-		// TODO use BS.Card -- but how to do collapse??
 		const color = error ? 'danger' : warning ? 'warning' : null;
 		
-		const titleText = _.isString(header) ? header : null;
+		// Is the title something we can use as a tooltip as well?
+		const titleText = _.isString(title) ? title : null;
 
-
-		let header = null;
+		// Header modifiers
 		let headerClasses = [];
 		if (onHeaderClick) headerClasses.push('btn btn-link');
 		if (color) {
@@ -58,7 +53,6 @@ class Card extends React.Component {
 			headerClasses.push(error ? 'text-white' : warning ? 'text-dark' : null)
 		}
 
-		
 		// Error or warning to show user?
 		const alert = (error && _.isString(error)) ? (
 			<Misc.Icon fa="exclamation-triangle" color={color} title={error} className="mr-2" />
@@ -66,13 +60,14 @@ class Card extends React.Component {
 			<Misc.Icon fa="exclamation-circle" color={color} title={warning} className="mr-2" />
 		) : null;
 
+		// Clickable header takes a caret to signify it's clickable
 		const caret = onHeaderClick ? (
 			<Misc.Icon className='pull-right' fa={`caret-${collapse ? 'down' : 'up'}`} />
 		) : null;
 
 		return (
-			<BSCard color={color} outline className={join(className, 'mb-3')} title={titleText}>
-				<CardHeader className={join(headerClasses)}>
+			<BSCard color={color} outline className={join(className, 'mb-3')}>
+				<CardHeader className={join(headerClasses)} onClick={onHeaderClick} title={titleText}>
 					{(glyph || icon) ? <Misc.Icon glyph={glyph} fa={icon} className="mr-2"/> : null}
 					{title ? <span className="mr-2">{title}</span> : null}
 					{alert}
@@ -93,56 +88,45 @@ class Card extends React.Component {
  *    children should be Misc.Card OR pass on ...other params to a Misc.Card. Otherwise the open/close clickers wont show.
  * @param {?Boolean} defaultOpen - Should all cards start open or closed? This is more normally set at the Card level.
  */
-const CardAccordion = ({widgetName, children, multiple, start, defaultOpen}) => {
+const CardAccordion = ({ children, multiple, defaultOpen}) => {
 	// NB: accordion with one child is not an array
-	if ( ! _.isArray(children)) {
-		children = [children];
-	}
-	if (defaultOpen && ! multiple) console.warn("CardAccordion.jsx - defaultOpen=true without multiple=true is odd.");
+	if (!_.isArray(children)) children = [children];
+
 	// filter null, undefined
-	children = children.filter(x => !! x);
-	// NB: React-BS provides Accordion, but it does not work with modular panel code. So sod that.
-	// TODO manage state
-	const wcpath = ['widget', widgetName || 'CardAccordion'];
-	const openPath = wcpath.concat('open');
-	let opens = DataStore.getValue(openPath); // type boolean[]
-	// Check if there's a predefined initial open state for each child
-	if ( ! opens) {
-		if (defaultOpen !== undefined) {
-			// start with all open (or closed)
-			opens = children.concat().fill(defaultOpen);
-		} else {
-			let explicitOpen = false;
-			opens = React.Children.map(children, (Kid, i) => {
-				if ( ! Kid.props) {
-					return false; // huh? seen Aug 2019 on Calstat
-				}
-				if (Kid.props.defaultOpen !== undefined) explicitOpen = true;
-				return !! Kid.props.defaultOpen;
-			});
-			if ( ! explicitOpen) opens = [true]; // default to first kid open
+	children = children.filter(x => !!x);
+
+	const [opens, setOpens] = useState([true]); // default to first child panel open
+
+	useEffect(() => {
+		if (defaultOpen && !multiple) {
+			console.warn("CardAccordion.jsx - defaultOpen=true without multiple=true is odd.");
 		}
-	}
-	if ( ! children) {
-		return (<div className='CardAccordion' />);
-	}
-	assert(_.isArray(opens), "Misc.jsx - CardAccordion - open not an array", opens);
-	const kids = React.Children.map(children, (Kid, i) => {
-		let collapse = ! opens[i];
-		let onHeaderClick = e => {
-			if ( ! multiple) {
-				// close any others
-				opens = [];
-			}
-			opens[i] = collapse;
-			DataStore.setValue(openPath, opens);
-		};
-		// clone with click
-		return React.cloneElement(Kid, {collapse, onHeaderClick: onHeaderClick});
-	});
+		if (defaultOpen !== undefined) {
+			// start with all open/closed
+			setOpens(children.concat().fill(defaultOpen));
+		} else {
+			// Child without props seen Aug 2019 on Calstat
+			setOpens(React.Children.map(children, ({props}) => (props && !!props.defaultOpen)));
+		}
+	}, []);
+
+	if (!children) return <div className='CardAccordion' />;
+
+	assert(_.isArray(opens), "Misc.jsx - CardAccordion - opens not an array", opens);
+
 	return (
 		<div className='CardAccordion'>
-			{kids}
+			{React.Children.map(children, (child, i) => {
+				let collapse = !opens[i];
+				let onHeaderClick = e => {
+					// Not in multiple mode ? Close all other cards
+					const newOpens = multiple ? opens.slice() : [];
+					newOpens[i] = collapse;
+					setOpens(newOpens);
+				};
+				// Clone child, adding collapse and header-click props
+				return React.cloneElement(child, {collapse, onHeaderClick});
+			})}
 		</div>
 	);
 };
