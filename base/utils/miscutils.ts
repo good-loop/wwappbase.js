f307fef34751fef35fbde3eb456235ae871bedc6
Daniel Winterstein <daniel@sodash.com>
2020-05-10 15:26:29 +0100

    chat

--- base/utils/miscutils.ts
+++ base/utils/miscutils.ts
@@ -1,3 +1,4 @@
+import Messaging from "../plumbing/Messaging";
 
 const randomPick = function<T>(array : T[]) : T
 {
@@ -17,6 +18,55 @@ const isMobile = ()  => {
 /**  */
 const isPortraitMobile = () => window.matchMedia("only screen and (max-width: 768px)").matches && window.matchMedia("(orientation: portrait)").matches;
 
+
+/**
+ * 
+ */
+const doShare = ({href,title,text}) => {
+	if ( ! navigator.share) {
+		console.warn("No share function");
+		
+		Messaging.notifyUser("Sharing link copied to clipboard");
+		return;
+	}
+	navigator.share({url:href,title,text});
+};
+
+function fallbackCopyTextToClipboard(text) {
+	var textArea = document.createElement("textarea");
+	textArea.value = text;
+	
+	// Avoid scrolling to bottom
+	textArea.style.top = "0";
+	textArea.style.left = "0";
+	textArea.style.position = "fixed";
+  
+	document.body.appendChild(textArea);
+	textArea.focus();
+	textArea.select();
+  
+	try {
+	  var successful = document.execCommand('copy');
+	  var msg = successful ? 'successful' : 'unsuccessful';
+	  console.log('Fallback: Copying text command was ' + msg);
+	} catch (err) {
+	  console.error('Fallback: Oops, unable to copy', err);
+	}
+  
+	document.body.removeChild(textArea);
+  }
+  function copyTextToClipboard(text) {
+	if (!navigator.clipboard) {
+	  fallbackCopyTextToClipboard(text);
+	  return;
+	}
+	navigator.clipboard.writeText(text).then(function() {
+	  console.log('Async: Copying to clipboard was successful!');
+	}, function(err) {
+	  console.error('Async: Could not copy text: ', err);
+	});
+  }
+
 // DEBUG hack
 window.miscutils = {
 	randomPick,
@@ -29,5 +79,6 @@ export {
 	randomPick,
 	sum,
 	isMobile,
-	isPortraitMobile
+	isPortraitMobile,
+	doShare
 };
