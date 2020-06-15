73c162c85b3d066293e298d01ea98d984bb65cd0
Roscoe <roscoe@winterwell.com>
2020-06-15 12:21:42 +0100

    Adblock check: use the more-suspicious "ads.good-loop.com" domain to trigger detection on more "nuanced" blockers

--- base/utils/DetectAdBlock.js
+++ base/utils/DetectAdBlock.js
@@ -7,7 +7,7 @@ const DetectAdBlock = ({path}) => {
 	const $script = document.createElement('script');
 	// Based on https://www.detectadblock.com/
 	// Adblockers are expected to always block js files with "ads" in the name
-	$script.setAttribute('src', 'https://as.good-loop.com/ads.js');
+	$script.setAttribute('src', 'https://ads.good-loop.com/ads.js');
 	
 	$script.onload = () => {
 		// If adblocker enabled, ads.js will not be able to create div with id #aiPai9th 
