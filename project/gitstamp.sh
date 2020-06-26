11f09cde030995b28009cbac1874c0edba904dcb
Daniel Winterstein <daniel@good-loop.com>
2020-06-26 08:29:44 +0100

    gitstamp-tweak

--- project/gitstamp.sh
+++ project/gitstamp.sh
@@ -2,15 +2,20 @@
 # Use-case: This shows what code the server is running
 
 LOGFILE=web/build/npm-compile.gitlog.txt
+
 echo '## ' `pwd` > $LOGFILE
 echo "" >> $LOGFILE 
 git status >> $LOGFILE 
 echo "" >> $LOGFILE 
 git log -1 >> $LOGFILE 
+
 echo "" >> $LOGFILE 
 echo "---------------" >> $LOGFILE 
 echo "" >> $LOGFILE 
+
 echo '## wwappbase.js' >> $LOGFILE
+echo "" >> $LOGFILE 
+git status >> $LOGFILE 
 echo "" >> $LOGFILE 
 git --git-dir ../wwappbase.js/.git log -1 >> $LOGFILE
 
