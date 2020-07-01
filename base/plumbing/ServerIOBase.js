13df323d60b8d341940766b0590cc2a4819e70f4
Daniel Winterstein <daniel@good-loop.com>
2020-07-01 12:53:43 +0100

    testas does portal too

--- base/plumbing/ServerIOBase.js
+++ base/plumbing/ServerIOBase.js
@@ -33,7 +33,7 @@ ServerIO.ENDPOINT_NGO = 'https://app.sogive.org/charity';
 ServerIO.ENDPOINT_TASK = 'https://calstat.good-loop.com/task';
 // ServerIO.ENDPOINT_TASK = 'http://localcalstat.good-loop.com/task';
 
-// TODO clean up other definitions inc PORTAL_DOMAIN
+// ServerIO.PORTAL_ENDPOINT = `${C.HTTPS}://${C.SERVER_TYPE}portal.good-loop.com`;
 ServerIO.PORTAL_ENDPOINT = 'https://portal.good-loop.com';
 
 ServerIO.MEDIA_ENDPOINT = `${C.HTTPS}://${C.SERVER_TYPE}uploads.good-loop.com/`;
