489c0217f8b8d66ee4c8a6912bab40e2c5faf51a
DA <daniel.appel.winterwell@gmail.com>
2020-07-01 12:53:50 +0100

    committing my WiP production publisher template script

--- template/production.server.project.builder.sh
+++ template/production.server.project.builder.sh
@@ -59,13 +59,76 @@ NPM_RUN_COMPILE_LOGFILE="/home/winterwell/.npm/_logs/npm.run.compile.for.$PROJEC
 ### Functions
 #######
 
-## Dependency Checks Functions
-# Human checklist:
-# 01. 'bob' is a globally available command -- and it is up-to-date
-# 02. 'jerbil' is a globally available command -- and it is up-to-date
-# 03. 'mvn' is a globally available command
-# 04. 'JAVA_HOME' is set correctly
-# 05. 'node' is version 12.x
-# 06. 'npm' is at the latest version
-# 07. 'wwappbase.js' directory is where it is expected (as defined earlier in this script)
-# 08. repository 'winterwell-code' is located in /home/winterwell/bobwarehouse/
+# Dependency Check Function - 'bob' is globally available
+function check_bob_exists {
+    if [[ $PROJECT_USES_BOB = 'yes' ]]; then
+        if [[ $(which bob) = '' ]]; then
+            printf "\nNo global installation of 'bob' was found. As 'root' please:\n\tnpm install -g java-bob\nAnd then as a regular user, please:\n\tbob\nand then retry this script\n"
+            exit 0
+        fi
+    fi
+}
+
+# Dependency Check Function - 'jerbil' is globally available
+function check_jerbil_exists {
+    if [[ $PROJECT_USES_JERBIL = 'yes' ]]; then
+        if [[ $(which jerbil) = '' ]]; then
+            printf "\nNo global installation of 'jerbil' was found. As 'root' please:\n\tnpm install -g jerbil-website\nAnd then as a regular user, please:\n\tjerbil\nAnd then retry this script\n"
+            exit 0
+        fi
+    fi
+}
+
+# Dependency Check Function - 'mvn' is globally available
+function check_maven_exists {
+    if [[ $PROJECT_USES_BOB = 'yes' ]]; then
+        if [[ $(which mvn) = '' ]]; then
+            printf "\nNo global installation of maven was found. As 'root' please:\n\tapt-get install maven\nAnd then retry this script\n"
+            exit 0
+        fi
+    fi
+}
+
+# Dependency Check Function - 'JAVA_HOME' is set correctly in this CLI environment
+function check_java_home {
+    if [[ $PROJECT_USES_BOB = 'yes' ]]; then
+        if [[ $(printf $JAVA_HOME) = '' ]]; then
+            printf "\nYou must have an export in this user's .bashrc file which set's JAVA_HOME to the path where your java is installed\n\n\texport JAVA_HOME=/usr/lib/jvm/java-11-openjdk-amd64\n\nand then\n\tsource ~/.bashrc\nAnd then retry this script\n"
+            exit 0
+        fi
+        if [[ $(printf $JAVA_HOME) != '/usr/lib/jvm/java-11-openjdk-amd64' ]]; then
+            printf "\nYour \$JAVA_HOME is not set to the correct path.  It should be /usr/lib/jvm/java-11-openjdk-amd64\n"
+            exit 0
+        fi
+    fi
+}
+
+# Dependency Check Function - nodejs is at version 12.x
+function check_nodejs_version {
+    if [[ $PROJECT_USES_NPM = 'yes' ]]; then
+        if [[ $(node -v | grep "v12") = '' ]]; then
+            printf "Either nodejs is not installed, or it is not at version 12.x.x\n"
+            exit 0
+        fi
+    fi
+}
+
+# Dependency Check Function - wwappbase.js is verified on disk
+function check_for_wwappbasejs_location {
+    if [[ $PROJECT_USES_WWAPPBASE_SYMLINK = 'yes' ]]; then
+        if [[ ! -d $WWAPPBASE_REPO_PATH_ON_SERVER_DISK ]]; then
+            printf "\nThe location of $WWAPPBASE_REPO_PATH_ON_SERVER_DISK could not be verified.  Please check this server's repositories and then check the variables regarding wwappbase.js at the top of this script.\n"
+            exit 0
+        fi
+    fi
+}
+
+# Dependency Check Function - bobwarehouse directory has discrete 'code' repository nested inside of it.
+function check_for_code_repo_in_bobwarehouse {
+    if [[ $PROJECT_USES_BOB = 'yes' ]]; then
+        if [[ ! -d /home/winterwell/bobwarehouse/code ]]; then
+            printf "\nYou must manually clone the 'winterwell-code' repo into the path /home/winterwell/bobwarehouse/  before attempting to use bob to compile your Java project\n"
+            exit 0
+        fi
+    fi
+}
