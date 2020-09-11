b4467c43ada56bd5dd68366af14bc80745611511
Daniel Winterstein <daniel@good-loop.com>
2020-09-11 11:54:32 +0100

    BG-simplified

--- base/components/BG.jsx
+++ base/components/BG.jsx
@@ -9,26 +9,31 @@ import React from 'react';
  * @param {?string} height defaults to auto, which should take its size from the children.
  * Note: don't use height:"100%" unless the surrounding element has a fixed height! Otherwise this will render a background of 0 height.
  */
-const BG = ({image, src, children, opacity=0.5, size='cover', height='auto', minHeight, fullscreen}) => {
+const BG = ({image, src, children, size='cover', fullscreen, opacity}) => {
 	if (size==='fit') size = "100% 100%";
 	if (image) src = image.url;
+
+	if ( ! fullscreen) {
+		// TODO opacity
+		// NB: position relative, so the (cc) credit can go bottom-right
+		return (<div style={{backgroundImage: `url('${src}')`, backgroundSize: size, position: image&&'relative'}}>
+			{children}
+			{image && <div className='img-credit'><small>{image.name} image (cc) by {image.author}</small></div>}
+		</div>);
+	}
+	// NB: z-index only works on positioned elements
 	let style= {
 		backgroundImage: `url('${src}')`,
 		backgroundSize: size,
-		height:"100%", width:"100%",
 		position: fullscreen? 'fixed' : 'absolute',
+		opacity,
 		top:0,left:0,right:0,bottom:0,
-		zIndex: -1,
-		opacity
+		zIndex: -1
 	};
-	// NB: the outer div with position relative avoids the whole thing being absolute
-	// NB: the middle outer div with position abs seems to be needed to properly get full-size (oddities seen in chrome August 2020)	
-	return (<div style={{position:'relative', width:'100%',minHeight, height}}>
-		{/* <div style={{position:'absolute', width:'100%', height, top:0, left:0}} > */}
-		<div style={style} />
-		<div style={{zIndex:100}}>{children}</div>
+	return (<>
+		<div className='BG-img' style={style} />
+		{children}
 		{image && <div className='img-credit'><small>{image.name} image (cc) by {image.author}</small></div>}
-	{/* </div> */}
-	</div>);
+	</>);
 };
 export default BG;
