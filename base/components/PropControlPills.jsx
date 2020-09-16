55150ecd9b9b56db56eceebed48f55b469a8aa06
Daniel Winterstein <daniel@good-loop.com>
2020-09-16 12:39:40 +0100

    pills

--- base/components/PropControlPills.jsx
+++ base/components/PropControlPills.jsx
@@ -11,7 +11,7 @@ import { useState } from 'react';
  * A list-of-strings editor, where the strings are drawn as discrete "pills"
  */
 const PropControlPills = ({storeValue, modelValueFromInput, path, prop, proppath, type, fcolor, saveFn}) => {
-	let pills = storeValue;
+	let pills = storeValue || [];
 
 	const removeTag = (tg) => {
 		let pills2 = pills.filter(t => t !== tg);
