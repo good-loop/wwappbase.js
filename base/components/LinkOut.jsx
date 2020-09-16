eecaecb6140516023aa2733f180690d4098e9e77
Daniel Winterstein <daniel@good-loop.com>
2020-09-16 17:27:02 +0100

    LinkOut-doc

--- base/components/LinkOut.jsx
+++ base/components/LinkOut.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 
 /**
- * A link to an external web page, which opens in a new tab
+ * Just a convenience for an `<a>` tag to an external (potentially untrustworthy e.g. it might do referrer tracking) web page, which opens in a new tab.
  * @param {!string} href
  */
-const LinkOut = ({href, children, ...props}) => <a href={href} target="_blank" rel="noreferrer" {...props} >{ children }</a>
+const LinkOut = ({href, children, ...props}) => <a href={href} target="_blank" rel="noopener" rel="noreferrer" {...props} >{ children }</a>
+
 
 export default LinkOut;
