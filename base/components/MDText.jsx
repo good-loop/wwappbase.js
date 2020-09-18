e16e9e8c602f38efe89db7f6d1a224529f5d9203
Roscoe <roscoe@winterwell.com>
2020-09-18 13:43:34 +0100

    MDText: replace invocation of String.replaceAll() with String.replace() which is better supported

--- base/components/MDText.jsx
+++ base/components/MDText.jsx
@@ -5,19 +5,19 @@ import Misc from './Misc';
 /**
  * Remove non-standard characters and render Markdown.
  * @param {!String} source text to render
- * @param {?Object} renderers node type: React component. This is merged with the default renderers. 
+ * @param {?Object} renderers node type: React component. This is merged with the default renderers.
  * The props passed to the component varies based on the type of node.
  */
-const MDText = ({source, renderers, escapeHtml=false}) => {
+const MDText = ({source, renderers, escapeHtml = false}) => {
 	// Misc.normalise is not a thing. Should this component be retired ??
 	let nsource = Misc.normalise(source);
-	nsource = nsource.replaceAll("<br/>","    \n"); // HACK - always support break tags
+	nsource = nsource.replace('<br/>', '    \n'); // HACK - always support break tags
 
 	// security: no onClick etc traps	
 	if ( ! escapeHtml) {
-		let bad = nsource.match(/<[^>]+\bon[a-zA-Z]+=/g,'');
+		let bad = nsource.match(/<[^>]+\bon[a-zA-Z]+=/g, '');
 		if (bad) {
-			console.warn("Dangerous content in markdown!",bad,nsource);		
+			console.warn("Dangerous content in markdown!", bad, nsource);
 		}
 		escapeHtml = true;
 	}
