a7724d182327ac400d5bd396b5255e1e366e9bc5
Roscoe <roscoe@good-loop.com>
2020-12-17 16:23:43 +0000

    Stripe API upgrade: still needs testing + cleanup of logging

--- base/components/PaymentWidget.jsx
+++ base/components/PaymentWidget.jsx
@@ -15,12 +15,15 @@ import { assert, assMatch } from '../utils/assert';
 import Misc from './Misc';
 import DataStore from '../plumbing/DataStore';
 import { loadStripe } from '@stripe/stripe-js';
+import ServerIO from '../../plumbing/ServerIO';
 
-// falsy value for SERVER_TYPE = production
-const stripeKey = (C.SERVER_TYPE) ?
+// Which Stripe API key to use?
+const stripeKey = (C.SERVER_TYPE) ? // SERVER_TYPE is falsy on production servers
 	'pk_test_RyG0ezFZmvNSP5CWjpl5JQnd' // test
 	: 'pk_live_InKkluBNjhUO4XN1QAkCPEGY'; // live
 
+// Dummy / special tokens for "test: pretend I paid", "pay by credit", "promotional free donation"
+// The server will catch these token IDs and route them appropriately instead of trying to collect from Stripe
 const SKIP_TOKEN = {
 	id: 'skip_token',
 	type: 'card',
@@ -33,8 +36,8 @@ const FREE_TOKEN = {
 	id: 'free_token',
 	type: 'free',
 };
-/** minimal transation amount by currency
-// table copied from https://stripe.com/docs/currencies (31/08/18) */
+
+/** Minimum transaction amount by currency: from https://stripe.com/docs/currencies (31/08/18) */
 const STRIPE_MINIMUM_AMOUNTS = {
 	'GBP': 0.5,
 	'USD': 0.75,
@@ -64,25 +67,39 @@ const STRIPE_MINIMUM_AMOUNTS = {
  * 	The token string is either a Stripe authorisation token, or one of the fixed special values (e.g. credit_token).
  * @param {?Boolean} testOption true/false to show/hide the pretend-I-paid option. Defaults to true on test or local.
  */
-const PaymentWidget = ({amount, onToken, recipient, email, usePaymentRequest, error, testOption}) => {
-	if ( ! amount) {
-		return null; // no amount, no payment
+const PaymentWidget = ({amount, onToken, recipient, email, usePaymentRequest, error, testOption, repeat}) => {
+	if (!amount) return null; // no amount, no payment
+
+	if (testOption === undefined) {
+		testOption = !C.isProduction();
 	}
-	if (testOption===undefined) {
-		testOption = ! C.isProduction();
-	}
+
+	// Promise containing the Stripe object
+	const [stripePromise, setStripePromise] = useState();
+	// Create once and persist so Stripe Elements don't complain about prop change on re-render
+	useEffect(() => setStripePromise(loadStripe(stripeKey)), [stripeKey]);
+
+	// The current PaymentIntent object
+	const [paymentIntent, setPaymentIntent] = useState(null);
+	// Get a new PaymentIntent if we don't have one, or update it if the payment amount changes
+	useEffect(() => {
+		// Stripe takes amount in "smallest unit of currency" - i.e. pence in GBP, cents in USD
+		const data = { action: 'getPaymentIntent', amount: Math.round(Money.value(amount) * 100) };
+		// If a payment intent exists, ask the server to update it with the new payment amount
+		if (paymentIntent) data.id = paymentIntent.id;
+
+		ServerIO.load('/stripe-paymentintent', {data}).then(({cargo}) => setPaymentIntent(cargo));
+		// Set null so form remains locked until we get the new/updated PaymentIntent	
+		setPaymentIntent(null);
+	}, [Money.value(amount)]);
+
 	Money.assIsa(amount, "PaymentWidget.jsx");
 	assMatch(onToken, Function, "PaymentWidget.jsx");
 	assMatch(recipient, String, "PaymentWidget.jsx");
 
-	// Money = £0? Then just a confirm button
+	// Amount to pay = £0? Then just a confirm button.
 	if (Money.value(amount) === 0) {
-		const payNothing = (event) => (
-			onToken({
-				...FREE_TOKEN,
-				email,
-			})
-		);
+		const payNothing = () => onToken({ ...FREE_TOKEN, email,	});
 		return (
 			<div className="PaymentWidget">
 				<button onClick={payNothing} className="btn btn-primary">Confirm Free Purchase</button>
@@ -90,24 +107,17 @@ const PaymentWidget = ({amount, onToken, recipient, email, usePaymentRequest, er
 		);
 	} // ./ £0
 
-	// Invoke the callback, with a minimal fake token that the servlet will catch
-	const skipAction = (event) => (
-		onToken({
-			...SKIP_TOKEN,
-			email,
-		})
-	);
-	const payByCredit = (event) => (
-		onToken({
-			...CREDIT_TOKEN,
-			email,
-		})
-	);
+	// Pretend user paid (test only): Invoke the callback, with a minimal fake token that the servlet will catch
+	const skipAction = () => onToken({ ...SKIP_TOKEN, email, });
+
+
 
 	// pay on credit??
 	let credit = Transfer.getCredit(); // ??This is kind of SoGive specific
 	if (credit && Money.value(credit) > 0) {
 		if (Money.value(credit) >= Money.value(amount)) {
+			// 
+			const payByCredit = () =>  onToken({ ...CREDIT_TOKEN, email, });
 			return (
 				<div className="section donation-amount">
 					<p>You have <Misc.Money amount={credit} /> in credit which will pay for this.</p>
@@ -117,24 +127,34 @@ const PaymentWidget = ({amount, onToken, recipient, email, usePaymentRequest, er
 		}
 	} // ./credit
 
-	const stripePromise = loadStripe(stripeKey);
+	// We need a PaymentIntent (which we get from the backend) before we can show the credit card form.
+	if (!paymentIntent || !paymentIntent.clientSecret) {
+		return (
+			<div className="section donation-amount">
+				<Misc.Loading text="Preparing your Stripe payment..." />
+			</div>
+		);
+	}
 	
 	return (
 		<div className="section donation-amount">
 			<Elements stripe={stripePromise}>
 				<StripeThingsFunctional onToken={onToken} amount={amount} credit={credit} recipient={recipient}
 					email={email} usePaymentRequest={usePaymentRequest} serverError={error}
+					clientSecret={paymentIntent && paymentIntent.clientSecret}
+					repeat={repeat}
 				/>
 			</Elements>
 
-			{error? <div className="alert alert-danger">{error}</div> : null}
+			{error ? <div className="alert alert-danger">{error}</div> : null}
 
-			{testOption? (
+			{testOption ? (
 				<small className="clear">
-					Test card no: 4000008260000000 (use any CVC and any future expiry date).
-					Stolen test card no: 4000000000009979.
-					Or
-					<button onClick={skipAction}>test: pretend I paid</button>
+					Test cards (use any CVC and any future expiry before 2070):<br/>
+					4000008260000000 (normal)<br/>
+					4000002760003184 (3D Secure auth for first transaction)<br/>
+					4000000000009979 (stolen)<br/>
+					Or <button onClick={skipAction}>test: pretend I paid</button>
 				</small>
 			) : null}
 		</div>
@@ -149,7 +169,7 @@ const PaymentWidget = ({amount, onToken, recipient, email, usePaymentRequest, er
  * "we provide the widgets and the host page can't touch your CC data".
  * It's conceivable we could pry that data out, but it's not a good idea.
  */
-const StripeThingsFunctional = ({ onToken, amount, credit, recipient, dfltEmail, usePaymentRequest, error: serverError, repeatAmount, repeatFreq, repeatEnd }) => {
+const StripeThingsFunctional = ({ onToken, amount, credit, recipient, dfltEmail, usePaymentRequest, error: serverError, repeat, clientSecret }) => {
 	// Stripe hooks methods, replacing the old-style provider wrappers that injected these as props
 	const stripe = useStripe();
 	const elements = useElements();
@@ -174,7 +194,7 @@ const StripeThingsFunctional = ({ onToken, amount, credit, recipient, dfltEmail,
 		browser-native payment request.
 	*/
 	useEffect(() => {
-		// Invoking component says not to use Payments API.
+		// Don't try to use Payments API unless the invoking component requests it.
 		if (!usePaymentRequest) {
 			setPaymentRequest(false)
 			return;
@@ -186,6 +206,7 @@ const StripeThingsFunctional = ({ onToken, amount, credit, recipient, dfltEmail,
 			residual = Money.sub(amount, credit);
 		}
 
+		/* TODO This probably needs updating - but it's dummied out with usePaymentRequest=false anyway */
 		const paymentRequestTemp = stripe.paymentRequest({
 			country: 'GB',
 			currency: (amount.currency || 'gbp').toLowerCase(),
@@ -224,52 +245,50 @@ const StripeThingsFunctional = ({ onToken, amount, credit, recipient, dfltEmail,
 
 	const handleSubmit = (event) => {
 		console.log("PaymentWidget - handleSubmit", event);
-		// Don't submit and cause a pageload!
+		// Don't let the browser submit and cause a pageload!
 		event.preventDefault();
 		// block repeat clicks, and clear old errors
 		setIsSaving(true);
 		setErrorMsg('');
 
-		const cardNumberElement = elements.getElement(CardNumberElement);
-		stripe.createPaymentMethod({ type: 'card', card: cardNumberElement})
-			.then(({ error, paymentMethod }) => {
-				if (error) console.log('ERROR from createPaymentMethod', error);
-				else console.log('SUCCESS from createPaymentMethod', paymentMethod);
-			});
-			return;
+		const paymentOptions = {
+			payment_method: {
+				card: elements.getElement(CardNumberElement),
+			},
+		};
 
+		// For repeating donations, tell Stripe we need off-session authorisation
+		// (i.e. can collect with customer not present) for future charges to this card.
+		if (repeat && repeat.amount && Money.value(repeat.amount) > 0) {
+			paymentOptions.setup_future_usage = 'off_session';
+		}
 
-		// MIGRATION TODO Is this still true?
-		// Within the context of `Elements`, this call to createToken knows which Element to
-		// tokenize, since there's only one in this group.
-		let tokenInfo = { type: "card" };
-		let ownerInfo = { name: username, email };
-
-		// get the token or reusable source from stripe
-		// see https://stripe.com/docs/sources/cards
-		// TODO update to https://stripe.com/docs/payments/payment-intents
-		stripe.createSource(tokenInfo, ownerInfo)		// Token(tokenInfo)
-			// then call custom processing (e.g. publish donation)
-			.then(({ token, source, error }) => {
-				if (source || token) {
-					onToken(source || token);
-				} else {
-					setErrorMsg(error && error.message);
-					setIsSaving(false);
+		stripe.confirmCardPayment(clientSecret, {
+			...paymentOptions
+		}).then(({paymentIntent, error}) => {
+			if (paymentIntent) {
+				// The payment went through! Pass the completed intent back to the invoking code.
+				console.log('*********** confirmCardPayment paymentIntent:', paymentIntent);	
+				onToken(paymentIntent);
+			} else {
+				// Something's gone wrong - handle the error.
+				console.log('*********** confirmCardPayment error:', error);
+				// Card errors are safe to show to the end-user.
+				if (error.type === 'card_error') {
+					setErrorMsg(error.message);
 				}
-			})
-			// on abject (eg. network) failure, mark the form as active again
-			.catch(() => setIsSaving(false));
+				// TODO Take action on other errors
+			}
+			setIsSaving(false);
+		});
 	} //./handleSubmit()
 
-
 	
-
 	// TODO an email editor if email is unset
 	return (
 		<Form onSubmit={(event) => handleSubmit(event)}>
 			<h4>Payment to {recipient}</h4>
-			<PaymentAmount amount={amount} repeatAmount={repeatAmount} repeatFreq={repeatFreq} repeatEnd={repeatEnd} />
+			<PaymentAmount amount={amount} repeat={repeat} />
 			{credit && Money.value(credit) > 0?
 				<FormGroup><Col md="12">
 					You have <Misc.Money amount={credit} /> in credit which will be used towards this payment.
@@ -316,20 +335,20 @@ const StripeThingsFunctional = ({ onToken, amount, credit, recipient, dfltEmail,
 	);
 } // ./StripeThingsClass
 
-const PaymentAmount = ({amount, repeatAmount, repeatFreq, repeatEnd}) => {
-	if ( ! repeatAmount) {
+const PaymentAmount = ({amount, repeat = {}}) => {
+	if (!repeat.amount) {
 		return <h4><Misc.Money amount={amount} /></h4>;
 	}
 		// TODO  {repeatEnd}
-	if (Money.value(repeatAmount) === Money.value(amount)) {
+	if (Money.value(repeat.amount) === Money.value(amount)) {
 		return (<>
-			<h4><Misc.Money amount={amount} /> {Donation.strRepeat(repeatFreq)} {repeatEnd}</h4>
+			<h4><Misc.Money amount={amount} /> {Donation.strRepeat(repeat.freq)} {repeatEnd}</h4>
 			<div>The regular payment can be cancelled at any time.</div>
 		</>);
 	}
 	return (<>
 		<h4><Misc.Money amount={amount} /> now.</h4>
-		<h4>Then <Misc.Money amount={repeatAmount} /> {Donation.strRepeat(repeatFreq)}.</h4>
+		<h4>Then <Misc.Money amount={repeat.amount} /> {Donation.strRepeat(repeat.freq)}.</h4>
 		<div>The regular payment can be cancelled at any time.</div>
 	</>);	
 };
