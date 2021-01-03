db0ac8abe485a59782a3111deca1cac88368b523
Daniel Winterstein <daniel@good-loop.com>
2021-01-03 20:57:58 +0000

    sync

--- base/components/Editor3ColLayout.jsx
+++ base/components/Editor3ColLayout.jsx
@@ -10,26 +10,27 @@ import ErrorBoundary from './ErrorBoundary';
 const Editor3ColLayout = ({children, showAll}) => {
 	const sz = getScreenSize();
 	if (children.length > 3) {
-		console.error("Editor3ColLayout - Truncating to 3", children);
+		console.error("Editor3ColLayout - Too many children", children);
 	}
-	const is3 = children.length > 2;
-	const leftNav = is3? children[0] : null;
-	const mainPane = is3? children[1] : children[0];
-	const rightPane = is3? children[2] : children[1];
+	// const is3 = children.length > 2;
+	// const leftNav = is3? children[0] : null;
+	// const mainPane = is3? children[1] : children[0];
+	// const rightPane = is3? children[2] : children[1];
 
-	// ?? make left also no-vertical scroll (but I think this is not as easy as for right)
-	let showLeft = (sz==='md' || sz==='lg' || sz==='xl') && is3;
-	let showRight = (sz==='lg' || sz==='xl') && rightPane;
+	// // ?? make left also no-vertical scroll (but I think this is not as easy as for right)
+	// let showLeft = (sz==='md' || sz==='lg' || sz==='xl') && is3;
+	// let showRight = (sz==='lg' || sz==='xl') && rightPane;
 
 	return (<div className='flex-row position-relative'>
 		{children}
 	</div>);
 };
 
+// margin-left 0 IF there is a LeftSidebar
 const MainPane = ({children}) => <Container><ErrorBoundary>{children}</ErrorBoundary></Container>
 
 const LeftSidebar = ({children}) => {
-	return <div className='mt-1' style={{maxWidth:"30%", position:"sticky", top:0}} >{children}</div>; // TODO use a slide-out tray if space is limited
+	return <div className='mt-1 mr-0' style={{maxWidth:"30%", position:"sticky", top:0}} >{children}</div>; // TODO use a slide-out tray if space is limited
 };
 const RightSidebar = ({children}) => {
 	return <div className='mt-1 flex-grow' style={{overflow:"scroll-y"}}><ErrorBoundary>{children}</ErrorBoundary></div>;
