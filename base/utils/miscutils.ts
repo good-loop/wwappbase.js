dc44bb0eff3bdfaa67ddb000de26a55796f0a773
Daniel Winterstein <daniel@good-loop.com>
2021-01-07 18:23:26 +0000

    sync

--- base/utils/miscutils.ts
+++ base/utils/miscutils.ts
@@ -439,7 +439,7 @@ export const ellipsize = function(s :string, maxLength :number) {
 	if ( ! s) return s;
 	if ( ! maxLength) maxLength = 140;
 	if (s.length <= maxLength) return s;
-	return s.substr(0, maxLength - 2)+' &hellip;';
+	return s.substr(0, maxLength - 1)+'…'; // NB: React doesn't render html entities, so lets use a unicode ellipsis.
 };
 
 /**
