269b4bf3d7f5e095113c015c4e0624fbd2a046cc
dodo721 <dodoworkspr@gmail.com>
2021-03-04 17:02:01 +0000

    Moved logic of impact hub ad filtering into Campaign.js

--- base/data/Campaign.js
+++ base/data/Campaign.js
@@ -4,6 +4,7 @@ import Enum from 'easy-enums';
 import DataClass from './DataClass';
 import C from '../CBase';
 import ActionMan from '../plumbing/ActionManBase';
+import List from './List';
 import DataStore, { getDataPath } from '../plumbing/DataStore';
 import deepCopy from '../utils/deepCopy';
 import { getDataItem, saveEdits } from '../plumbing/Crud';
@@ -86,5 +87,101 @@ Campaign.hideCharities = campaign => {
 	return hideCharitiesArr;
 };
 
+/**
+ * Get a list of adverts that the impact hub should show for this campaign
+ * @param {Campaign} topCampaign the subject campaign
+ * @param {String} status
+ * @param {?Campaign[]} campaigns any other campaigns with data to use (for advertisers or agencies)
+ * @param {?Advert[]} presetAds use a preset list of ads instead of fetching ourselves
+ * @returns {Advert[]} adverts to show
+ */
+Campaign.advertsToShow = (topCampaign, campaigns, presetAds) => {
+    let ads = [];
+
+    let {
+        status,
+        'gl.status': glStatus,
+        showNonServed,
+	} = DataStore.getValue(['location', 'params']) || {};
+    if ( ! status) status = (glStatus || C.KStatus.PUB_OR_ARC);
+
+    if (presetAds) {
+        ads = presetAds;
+    } else {
+        // Get ads for top campaign
+        let q = SearchQuery.setProp(new SearchQuery(), "campaign", topCampaign.id).query;
+        let pvAds = ActionMan.list({type: C.TYPES.Advert, status, q});
+        if (pvAds.value) {
+            List.hits(pvAds).forEach(ad => {
+                ads.push(ad);
+            });
+        }
+        // Get ads for associated campaigns
+        campaigns && campaigns.forEach(campaign => {
+            let qOtherAds = SearchQuery.setProp(new SearchQuery(), "campaign", campaign.id).query;
+            let pvOtherAds = ActionMan.list({type: C.TYPES.Advert, status, qOtherAds});
+            if (pvOtherAds.value) {
+                List.hits(pvOtherAds).forEach(ad => {
+                    ads.push(ad);
+                });
+            }
+        });
+    }
+
+    // Merge all hide advert lists together from all campaigns
+    if (!topCampaign.hideAdverts) topCampaign.hideAdverts = {};
+    campaigns && campaigns.forEach(c => {
+        if (c.hideAdverts) {
+            Object.keys(c.hideAdverts).forEach(hideAd => {
+                if (c.hideAdverts[hideAd]) topCampaign.hideAdverts[hideAd] = true;
+            });
+        }
+    });
+
+    // Filter ads using hide list
+    ads = ads.filter(ad => ! topCampaign.hideAdverts[ad.id]);
+	console.log("Hiding: ",campaign && campaign.hideAdverts);
+
+    /////////////////////////////////////////////////////////
+    // TODO - REMOVE ad by campaign sorting
+    // Maintaining for now until existing pages are adjusted
+    /////////////////////////////////////////////////////////
+    let sampleAd4Campaign = {};
+	ads.forEach(ad => {
+        // Skip never-served ads
+        if (!ad.hasServed && !ad.serving && !showNonServed) return;
+		let cname = campaignNameForAd(ad);
+		if (sampleAd4Campaign[cname]) {
+			let showcase = ad.campaignPage && ad.campaignPage.showcase;
+			// Prioritise ads with a start and end time attached
+			let startProvided = !sampleAd4Campaign[cname].start && ad.start;
+			let endProvided = !sampleAd4Campaign[cname].end && ad.end;
+			// If the ad cannot provide a new value for start or end, skip it
+			if (!startProvided && !endProvided && !showcase) {
+				return;
+			}
+		}
+		//if (!ad.videos || !ad.videos[0].url) return;
+		sampleAd4Campaign[cname] = ad;
+	});
+    const sampleAds = Object.values(sampleAd4Campaign);
+    
+    return sampleAds;
+};
+
+/**
+ * @param {!Advert} ad 
+ * @returns {!string} Can be "unknown" to fill in for no-campaign odd data items
+ */
+const campaignNameForAd = ad => {
+	if (!ad.campaign) return "unknown";
+	// HACK FOR TOMS 2019 The normal code returns 5 campaigns where there are 3 synthetic campaign groups
+	// Dedupe on "only the first josh/sara/ella campaign" instead
+	if (ad.vertiser === 'bPe6TXq8' && ad.campaign && ad.campaign.match(tomsCampaigns)) {
+		let cname = ad.campaign.match(tomsCampaigns)[0];
+		return cname;
+	}
+	return ad.campaign;
+};
 
 export default Campaign;
