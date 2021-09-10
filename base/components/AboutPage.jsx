58c48f636cf19650df958d357598ced8173228bb
Roscoe <roscoe@good-loop.com>
2021-09-10 12:52:14 +0100

    Clean up standard about page

--- base/components/AboutPage.jsx
+++ base/components/AboutPage.jsx
@@ -73,6 +73,63 @@ export const addFunderCredit = funder => {
 
 // TODO sponsors
 
+/** Well-formatted list of funders */
+const FunderCredits = ({funders}) => {
+	let textList;
+
+	if (!funders.length) textList = 'our funders';
+	else if (funders.length === 1) textList = funders[0];
+	else if (funders.length === 2) textList = funders.join(' and ');
+	else textList = funders.reduce((acc, funder, i) => {
+		if (i === 0) return funder;
+		if (i === funders.length - 1) return (acc + ', and ' + funder);
+		return (acc + ', ' + funder);
+	});
+
+	return <p>We are grateful to {textList} for their support.</p>
+};
+
+
+const MusicCredits = ({musicCredits}) => {
+	if (!musicCredits.length) return null;
+
+	return <>
+		<p>This app uses music from:</p>
+		<ul>
+			{musicCredits.map(credit => <li><LinkOut href={credit.url}>{credit.name} by {credit.author}</LinkOut></li>)}
+		</ul>
+	</>;
+}
+
+
+const ccText = 'This app uses Creative Commons images from various sources'
+
+const ImageCredits = ({imageCredits}) => {
+	if (!imageCredits.length) return <p>{ccText}.</p>;
+
+	return <>
+		<p>{ccText}:</p>
+		<ul>
+			{imageCredits.map(credit => <li><LinkOut href={credit.url}>{credit.name} by {credit.author}</LinkOut></li>)}
+		</ul>
+	</>
+};
+
+
+const dataText = 'This app uses data from various sources'
+
+const DataCredits = ({dataCredits}) => {
+	if (!dataCredits.length) return <p>{dataText}.</p>;
+
+	return <>
+		<p>{dataText}:</p>
+		<ul>
+			{dataCredits.map(credit => <li><LinkOut href={credit.url}>{credit.name} by {credit.author}</LinkOut></li>)}
+		</ul>
+	</>;
+};
+
+
 /**
  * 
  * @param {*} param0 
@@ -81,33 +138,26 @@ const AboutPage = () => {
 	let website = C.app.website; // ?? default to top-level domain
 	return (
 		<div className="AboutPage">
-			<h1>About {C.app.name}</h1>
-			{C.app.logo? <img src={C.app.logo} className="img-thumbnail logo-large pull-right" /> : null}
+			<h1>
+				{C.app.logo ? <img src={C.app.logo} className="img-thumbnail logo-small pull-left mr-4" /> : null}
+				About {C.app.name}
+			</h1>
+			
 
 			<p>Please see our website for more information on {C.app.name}: <a href={website}>{website}</a></p>
 
-			{C.app.facebookAppId? <a href={'https://www.facebook.com/games/?app_id='+C.app.facebookAppId}><Misc.Logo service="facebook" /> facebook</a> : null}
+			{C.app.facebookAppId ? (
+				<a href={`https://www.facebook.com/games/?app_id=${C.app.facebookAppId}`}>
+					<Misc.Logo service="facebook" size="small" /> Our Facebook page
+				</a>)
+			: null}
+
+			<FunderCredits funders={FUNDER_CREDITS} />
+			<ImageCredits imageCredits={IMAGE_CREDITS} />
+			<MusicCredits musicCredits={MUSIC_CREDITS} />
+			<DataCredits dataCredits={DATA_CREDITS} />
 
 			<p>Software version: <i>{JSON.stringify(C.app.version || 'alpha')}</i></p>
-
-			<p>We are grateful to {FUNDER_CREDITS.length? FUNDER_CREDITS.join(", ") : "our funders"} for their support.</p>
-
-			<p>This app uses Creative Commons images from various sources</p>
-			
-			{IMAGE_CREDITS.map(image => <LinkOut href={image.url}>{image.name} by {image.author}</LinkOut>)}			
-
-			{MUSIC_CREDITS.length &&
-				<div>
-					<p>This app uses music from:</p>
-					{MUSIC_CREDITS.map(image => <LinkOut href={image.url}>{image.name} by {image.author}</LinkOut>)}			
-				</div>
-			}
-
-			<p>This app uses data from various sources:</p>
-			<ul>
-				{DATA_CREDITS.map(dataset => <LinkOut href={dataset.url}>{dataset.name} by {dataset.author}</LinkOut>)}				
-			</ul>
-
 		</div>
 	);
 };
