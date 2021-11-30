d4c645886996dc501f181a5076b9f83b877527f3
Daniel Winterstein <daniel@good-loop.com>
2021-11-30 20:53:59 +0000

    only-seconds

--- base/components/PropControls/PropControlDt.jsx
+++ base/components/PropControls/PropControlDt.jsx
@@ -4,9 +4,7 @@ import { Input, InputGroup } from 'reactstrap';
 import { registerControl } from '../PropControl';
 
 // See TUnit
-const options = {MILLISECOND: 'msec', SECOND: 'sec', MINUTE: 'min', HOUR: 'hour', DAY: 'day', WEEK: 'week', MONTH: 'month', YEAR: 'year'};
-
-const domOptions = Object.entries(options).map(([option, label]) => <option key={option} value={option}>{label}</option>)
+const label4TUnit = {MILLISECOND: 'msec', SECOND: 'second', MINUTE: 'minute', HOUR: 'hour', DAY: 'day', WEEK: 'week', MONTH: 'month', YEAR: 'year'};
 
 // Base for a dummy event with dummy functions so we don't get exceptions when trying to kill it
 // TODO Copy-paste from PropControlUpload.jsx - factor out?
@@ -17,7 +15,8 @@ const fakeEvent = {
 };
 
 
-const PropControlDt = ({prop, storeValue, onChange}) => {
+const PropControlDt = ({prop, storeValue, onChange, unitOptions}) => {
+	// NB: Uses two inputs, which get presented upwards as if one thing
 	const onChangeInner = ({ n, unit }) => {
 		const newVal = { ...storeValue };
 		if (n) newVal.n = n;
@@ -26,11 +25,17 @@ const PropControlDt = ({prop, storeValue, onChange}) => {
 		onChange({...fakeEvent, target: { value: newVal }});
 	};
 
+	if ( ! unitOptions) unitOptions = Object.keys(label4TUnit);
+
+	// Minor todo: this could look a bit prettier with a BS input append
 	return <InputGroup inline>
 		<Input type="number" name={`${prop}-n`} value={storeValue.n} onChange={(e) => onChangeInner({n: e.target.value})} />
-		<Input type="select" name={`${prop}-unit`} value={storeValue.unit} onChange={(e) => onChangeInner({unit: e.target.value})} default="SECOND">
-			{domOptions}
-		</Input>
+		{unitOptions.length > 1?
+			<Input type="select" name={`${prop}-unit`} value={storeValue.unit} onChange={(e) => onChangeInner({unit: e.target.value})} default="SECOND">
+				{unitOptions.map(option => <option key={option} value={option}>{label4TUnit[option] || option}</option>)}
+			</Input>
+			: <span> {label4TUnit[unitOptions[0]] || unitOptions[0]}</span>
+		}
 	</InputGroup>;
 };
 
