8ce63aa1f7dde293e2cddd98c712ecb649b69b80
dodo721 <dodoworkspr@gmail.com>
2021-12-03 16:14:33 +0000

    updates

--- base/components/GoodLoopUnit.jsx
+++ base/components/GoodLoopUnit.jsx
@@ -232,7 +232,7 @@ const GoodLoopUnit = ({vertId, css, size = 'landscape', status, play = 'onvisibl
 
 	return (
 		<div className="goodLoopContainer" style={dims} ref={receiveContainer} id={vertId}>
-			<iframe key={unitKey} frameBorder={0} scrolling="auto" style={{width: '100%', height: '100%'}} ref={receiveFrame} />
+			<iframe key={unitKey} frameBorder={0} scrolling="auto" style={{width: '100%', height: '100%'}} ref={receiveFrame} aria-label="Good-Loop ad"/>
 		</div>
 	);
 };
