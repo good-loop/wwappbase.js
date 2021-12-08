6acad964e9a3018078f51cdf96d45466f8dc0368
Roscoe <roscoe@good-loop.com>
2021-12-08 15:09:42 +0000

    Remove Misc.PropControl to fix dependency loop

--- base/components/PropControls/PropControlUrl.jsx
+++ base/components/PropControls/PropControlUrl.jsx
@@ -1,27 +1,7 @@
 import React from 'react';
 
 import { FormControl, registerControl } from '../PropControl';
-
-/** Default validator for URL values*/
-const urlValidator = ({value, props}) => {
-	// no URL is not inherently an error
-	if (!value) return;
-	// Protocol-relative URLs are fine!
-	if (value.startsWith('//')) value = 'https:' + value;
-
-	try {
-		const urlObject = new URL(value);
-
-		if (urlObject.protocol !== 'https:') {
-			return {
-				status: (props.https ? 'error' : 'warning'),
-				message: 'Please use https for secure URLs'
-			};
-		}
-	} catch (e) {
-		return { status: 'error', message: 'This is not a valid URL' };
-	}
-};
+import { urlValidator } from './validators';
 
 
 // TODO Big similarities between url, img and uploader types - more code reuse?
@@ -34,23 +14,10 @@ const PropControlUrl = ({https, prop, value, storeValue, ...rest}) => (
 );
 
 
-// registerControl({
-// 	type: 'url',
-// 	$Widget: PropControlUrl,
-// 	validator: urlValidator
-// });
+registerControl({
+	type: 'url',
+	$Widget: PropControlUrl,
+	validator: urlValidator
+});
 
-// Expose for other PropControls like imgUpload
-// TODO Should validators be exposed under a standard name?
-export { urlValidator };
-
-// TODO Is this the Right Way?
-// We can't call registerControl here because - due to a dependency loop - it may not exist yet.
-// So we let PropControl.jsx receive a specification for each new control type & register them itseld.
-export const specs = [
-	{
-		type: 'url',
-		$Widget: PropControlUrl,
-		validator: urlValidator
-	}
-];
+export default {};
