faebc7f7f58c48cb2b579a2e692a88d8ccf75000
dodo721 <dodoworkspr@gmail.com>
2022-01-06 18:29:49 +0000

    Updates

--- base/utils/DetectAdBlock.js
+++ base/utils/DetectAdBlock.js
@@ -12,37 +12,48 @@ const doDetect = () => {
 	// Adblockers are expected to always block js files with "ads" in the name
 	$script.setAttribute('src', 'https://ads.good-loop.com/ads.js');
 	
-	//console.log("[ADBLOCK]","Checking ad block status...");
+	console.log("[ADBLOCK]","Checking ad block status...");
 
 	let pv = PromiseValue.pending();
 	$script.onload = () => {
-		//console.log("[ADBLOCK]","Ad script loaded!");
+		console.log("[ADBLOCK]","Ad script loaded!");
 		// If adblocker enabled, ads.js will not be able to create div with id #aiPai9th 
 		const adBlockEnabled = ! document.getElementById('aiPai9th');
-		//console.log("[ADBLOCK]","Ad script embedded!");
+		console.log("[ADBLOCK]","Ad script embedded!");
 		// DataStore.setValue(path, adBlockEnabled);
 		pv.resolve(adBlockEnabled);
 	};
 
 	$script.onerror = () => {
-		//console.log("[ADBLOCK]","Could not load test ad script. Checking general internet...");
+		console.log("[ADBLOCK]","Could not load test ad script. Checking general internet...");
 		// We might not be connected to internet at all - make another check
 		const $img = document.createElement('img');
 		$img.setAttribute('id', 'adblockTesterImg');
 		$img.setAttribute('src', 'https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png');
 
+		// Google's logo should be snappy, and we don't want to wait ages to confirm an adblock - can afford some error
+		setTimeout(() => {
+			if (!pv.resolved && !pv.error) {
+				// No internet!
+				pv.reject("offline");
+				console.log("[ADBLOCK]","Could not load general internet! We are offline");
+			}
+		}, 1500);
+
 		$img.onload = () => {
-			//console.log("[ADBLOCK]","Loaded general internet! Ads are blocked");
+			// Don't escape this function - allows for correcting false readings
+			console.log("[ADBLOCK]","Loaded general internet! Ads are blocked");
 			// Image loaded so internet exists but ads were blocked
 			// DataStore.setValue(path, true);
 			pv.resolve(true);
 		}
 
 		$img.onerror = () => {
-			//console.log("[ADBLOCK]","Could not load general internet! We are offline");
+			// Escape this if already timed out
+			if (pv.resolved || pv.error) return;
+			console.log("[ADBLOCK]","Could not load general internet! We are offline");
 			// We cannot load anything - no internet
-			// "null" equates to false for showing adblock popups but also can be null-tested for no internet
-			pv.resolve(null);
+			pv.reject("offline");
 		}
 
 	};
