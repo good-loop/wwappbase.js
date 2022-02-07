1c67db038eeb61bd7be60da2a2fb727e202cb896
Roscoe <roscoe@good-loop.com>
2022-02-07 19:06:04 +0000

    DynImg component up-to-date with support for URLs that don't fit in 255 characters of base64

--- base/components/DynImg.jsx
+++ base/components/DynImg.jsx
@@ -3,12 +3,8 @@ import { useState } from 'react';
 import { useRef } from 'react';
 import ImageObject from '../data/ImageObject';
 
-
-// Where should we request cached/resized images? Use the media cluster which matches the current local/test/prod site.
-let domainPrefix = window.location.host.match(/^(local|test)/);
-domainPrefix = domainPrefix ? domainPrefix[0] : '';
-domainPrefix = ''; // use the live cache
-const mediaCacheBase = `${window.location.protocol}//${domainPrefix}media.good-loop.com/uploads/mediacache/`;
+// TODO Make MEDIA_ENDPOINT standard in ServerIOBase
+const BASE_URL = `${ServerIO.ENDPOINT_MEDIA}/uploads/mediacache/`;
 
 
 /**
@@ -30,7 +26,7 @@ const DynImg = ({src, msrc, image, ...props}) => {
 	// work out the width
 	let [width, setWidth] = useState();
 	let ref = useRef();
-	if ( ! width) {
+	if (!width) {
 		// Set img src to instant-loading placeholder data-URL to probe size without loading anything
 		_src = transparentPixel;
 		if (ref.current) {
@@ -52,16 +48,20 @@ const DynImg = ({src, msrc, image, ...props}) => {
 			// restore the image's original inline width rule
 			if ( ! existingWidth) {
 				$img.style.width = inlineWidth;
-			}					
+			}
 		}
 	}
 	// wrap url
 	if (width) {
 		// Get scaled + cached image URL and set it on the <img>
-		_src = wrapUrl(src, width);
+		// Bump resolution a little to reduce chances of fuzziness
+		_src = wrapUrl(src, width * 1.25);
 	}
 	return <img ref={ref} src={_src} {...props} />;
 };
+
+
+
 
 
 // Let's quantise image sizes into 360px intervals (ie, neatly matched to common phone screen widths) + some tiny ones for good measure.
@@ -71,32 +71,127 @@ const sizes = [ 2160, 1800, 1440, 1080, 720, 360, 180, 90 ];
 /** A 1x1 transparent PNG for use as a placeholder src */
 const transparentPixel = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEUAAACnej3aAAAAAXRSTlMAQObYZgAAAApJREFUCNdjYAAAAAIAAeIhvDMAAAAASUVORK5CYII=';
 
-/** NB: Copy pasta from dynimg.js
- * Take a (hopefully image) URL and wrap it in a request to MediaCacheServlet, with optional scaling. 
- * @param {!string} src url, can be relative
- * @param {!number} width
- * */
-function wrapUrl(src, width) {
-	// resolve relative URLs
-	const url = new URL(src, document.location);
 
-	// use e.g. data: urls uncached
-	if (!url.protocol.match(/http/)) return src;
 
-	// base64web-encode URL for use as filename
-	const urlEncoded = btoa(url.href).replace('+', '-').replace('/', '_');
-	let extension = url.pathname.match(/\.[^.]+$/)[0];
-	
+/**
+ * Below code copy-pasted from adunit utils.js and CommonWidgets.jsx
+ * TODO: Figure out how to reconcile adunit and wwappbase so code like this can be shared.
+ */
+
+/** Parse out the hash of an image URL and check for a "crop to X% when displaying in a circle" marker */
+const getCcrop = (url) => {
+	try {
+		// Put a protocol on protocol-relative URLs to enable parsing...
+		const addProtocol = url.match(/^\/\//) ? 'https:' : '';
+		const cropMatch = parseUrl(addProtocol + url).hash.match(/ccrop\:(\d+)/);
+		if (cropMatch && cropMatch[1]) {
+			return parseInt(cropMatch[1]); // Found a ccrop:XX marker! Crop to XX% if this BGImg is circular
+		}
+	} catch (e) {}
+}
+
+/**
+ * "Cyrb53" hash function copy-pasted from StackOverflow:
+ * https://stackoverflow.com/questions/7616461/generate-a-hash-from-string-in-javascript
+ * Is it good? Well, people seem to trust it. More importantly it's very small.
+ * Variant which returns full 64-bit hash as hex string instead of 53-bit truncated number.
+ * @param {string} str Input string
+ * @return 8 hex characters
+ */
+ const cyrb = function(str) {
+	let h1 = 0xad54900d, h2 = 0x41c6ce57; // just magic numbers
+	for (let i = 0, ch; i < str.length; i++) {
+			ch = str.charCodeAt(i);
+			h1 = Math.imul(h1 ^ ch, 2654435761);
+			h2 = Math.imul(h2 ^ ch, 1597334677);
+	}
+	h1 = Math.imul(h1 ^ (h1>>>16), 2246822507) ^ Math.imul(h2 ^ (h2>>>13), 3266489909);
+	h2 = Math.imul(h2 ^ (h2>>>16), 2246822507) ^ Math.imul(h1 ^ (h1>>>13), 3266489909);
+	return (h2>>>0).toString(16).padStart(8,0) + (h1>>>0).toString(16).padStart(8,0);
+};
+
+/**
+ * Extend the output of our hash function to arbitrary length.
+ * Q: Does this really reduce likelihood of collision?
+ * It seems like it should but I don't have a proof.
+ * If we have "string-1-with-hash-a" and "string-2-with-colliding-hash-a"...
+ * ...then the next iteration will only ALSO collide if "[hash-a]string-1-with-hash-a"
+ * and "[hash-a]string-2-with-colliding-hash-a" also have the same hash. Which is
+ * astronomically unlikely... right?
+ */
+const urlToLongHash = (str, length) => {
+	let hash = '';
+	while (hash.length < length) {
+		hash = hash + cyrb(hash + str);
+	}
+	return hash.substring(0, length);
+};
+
+
+
+/**
+ * Avoid cross-domain complaints from eg Google by passing requests for resources
+ * outside the Good-Loop domain to the media server's automatic cache
+ * @param {String} urlString A media URL
+ * @param {Number} width The approximate width the resource is to be displayed at, as a percentage of adunit width
+ * @returns {String} A URL which will retrieve a cached copy of the file from the Good-Loop media server
+ */
+ const wrapUrl = React.memo((urlString, width) => {
+	if (!urlString) return null;
+	// Put a protocol on protocol-relative URLs to enable parsing
+	if (urlString.match(/^\/\//)) {
+		urlString = 'https:' + urlString;
+	}
+
+	const url = parseUrl(urlString);
+
+	// Check for a "Do not rescale!" marker in the hash
+	let noscale = url.hash.match(/\bnoscale\b/);
+		
+	// TODO Use window.devicePixelRatio to adjust size when on mobile
+
+	// does the invoking code ask for a particular size? (Don't request resized SVGs)
 	let sizeDir = '';
-	if (width && !extension.match(/\.svg/)) {
+	if (!noscale && width && !url.pathname.match(/\.svg/)) {
+		// Check for a for "get a larger or smaller resize than normal" multiplier in the hash
+		let multiplier = 1;
+		try {
+			let newMul = url.hash.match(/\b(\d+(\.\d*)?)x\b/);
+			if (newMul) newMul = Number.parseFloat(newMul[1]);
+			if (newMul) multiplier = newMul;
+		} catch (e) { }
+
+		let targetSize = (width ? width : height) * multiplier;
 		// Step down through quantised image widths & find smallest one bigger than estimated pixel size
 		let qWidth = sizes[0];
-		for (let i = 0; i < sizes.length && sizes[i] >= width; i++) {
+		for (let i = 0; i < sizes.length && sizes[i] >= targetSize; i++) {
 			qWidth = sizes[i];
 		}
 		sizeDir = `scaled/w/${qWidth}/`;
 	}
-	return mediaCacheBase + sizeDir + urlEncoded  + extension + '?from=good-loop-ad-unit';
-};
+	
+	if (!sizeDir && url.hostname.match(/media.good-loop.com$/)) return urlString; // Our media domain? Use it uncached - unless resize was requested
+	if (!url.protocol.match(/http/)) return urlString; // Not HTTP (eg data: url)? Use it uncached
+
+	// preserve extension because MIME-type at the server side is going to be based on wild filename-based guesses
+	const extension = url.pathname.match(/\.[^.]+$/);
+	// This is going to be used as a filename/URL, so '/' in the encoded string is unsafe
+	// However, there's a URL + filesystem-safe base64 standard we can transform to easily
+	// See https://tools.ietf.org/html/rfc4648#section-5
+	let filename = btoa(urlString).replace('+', '-').replace('/', '_') + extension;
+
+	let params = '?from=good-loop-ad-unit';
+
+	// The base64-encoded filename may be longer than the 255-character limit imposed by EXT4.
+	// In this case - use a hash of the URL as the filename, and explicitly tell the servlet
+	// the URL to fetch in the case of a cache miss.
+	if (filename.length > 250) { // allow a safety margin - longest name on media cluster is 220 characters.
+		// 16 hex characters = 128 bits = probability of collision among 1 trillion hashes too small to store in an IEEE-754 float. Probably enough.
+		filename = urlToLongHash(url, 16) + extension;
+		params += '&src=' + encodeURIComponent(url);
+	}
+
+	return mediaCacheBase + '/uploads/mediacache/' + sizeDir + filename + params;
+});
 
 export default DynImg;
