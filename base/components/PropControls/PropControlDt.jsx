caaa79f9467f67fbc65e33073b97c0254e547f94
Roscoe <roscoe@good-loop.com>
2022-02-08 18:35:30 +0000

    PropControlDt: Rework internal state management so Dt object always receives a TUnit

--- base/components/PropControls/PropControlDt.jsx
+++ base/components/PropControls/PropControlDt.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Input, InputGroup } from 'reactstrap';
 
 import { registerControl } from '../PropControl';
@@ -14,28 +14,31 @@ const fakeEvent = {
 	cooked: true, // Signal PropControl wrapper code NOT to call setRawValue
 };
 
+/**
+ * See Dt.java - this control bundles two inputs (numeric for count, drop-down for unit) into a time-duration editor.
+ */
+const PropControlDt = ({prop, storeValue, onChange, unitOptions = Object.keys(label4TUnit)}) => {
+	// Use controlled inputs so their state is in-scope here - but don't bind them directly to DataStore
+	const [nVal, setNVal] = useState(storeValue.n);
+	// Default to unit=SECOND if available
+	const [unitVal, setUnitVal] = useState(storeValue.unit || (() => unitOptions.find(a => a === 'SECOND') ? 'SECOND' : unitOptions[0]));
 
-const PropControlDt = ({prop, storeValue, onChange, unitOptions}) => {
-	// NB: Uses two inputs, which get presented upwards as if one thing
-	const onChangeInner = ({ n, unit }) => {
+	// When the inputs change, synthesise an input-change event to pass up to PropControl and DataStore
+	useEffect(() => {
 		const newVal = { ...storeValue };
-		if (n) newVal.n = n;
-		if (unit) newVal.unit = unit;
-
+		newVal.n = nVal;
+		newVal.unit = unitVal;
 		onChange({...fakeEvent, target: { value: newVal }});
-	};
+	}, [nVal, unitVal]);
 
-	if ( ! unitOptions) unitOptions = Object.keys(label4TUnit);
+	// Disable the dropdown (but keep it present) if there's only one option
+	const unitDisable = unitOptions.length <= 1;
 
-	// Minor todo: this could look a bit prettier with a BS input append
 	return <InputGroup>
-		<Input type="number" name={`${prop}-n`} value={storeValue.n} onChange={(e) => onChangeInner({n: e.target.value})} />
-		{unitOptions.length > 1?
-			<Input type="select" name={`${prop}-unit`} value={storeValue.unit} onChange={(e) => onChangeInner({unit: e.target.value})} default="SECOND">
-				{unitOptions.map(option => <option key={option} value={option}>{label4TUnit[option] || option}</option>)}
-			</Input>
-			: <span> {label4TUnit[unitOptions[0]] || unitOptions[0]}</span>
-		}
+		<Input type="number" name={`${prop}-n`} value={nVal} onChange={(e) => setNVal(e.target.value)} />
+		<Input type="select" name={`${prop}-unit`} value={unitVal} onChange={(e) => setUnitVal(e.target.value)} disabled={unitDisable}>
+			{unitOptions.map(option => <option key={option} value={option}>{label4TUnit[option] || option}</option>)}
+		</Input>
 	</InputGroup>;
 };
 
