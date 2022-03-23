1c5b0de5df3d9e877a6e2dff32d3fbee2f46edd0
dodo721 <dodoworkspr@gmail.com>
2022-03-23 17:13:27 +0000

    NGO image handling

--- base/components/NGOImage.jsx
+++ base/components/NGOImage.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import DataStore from "../plumbing/DataStore";
 import NGO from "../data/NGO";
 import { assert } from "../utils/assert";
@@ -6,45 +6,87 @@ import BG from "./BG";
 
 /**
  * Displays an image from the NGO. If no NGO is provided, acts as a normal image
- * By default will check which images have already been displayed on the page and choose the next in the list
+ * CURRENT - imgIdx is required
+ * TODO BEHAVIOUR - By default will check which images have already been displayed on the page and choose the next in the list
  * @param {NGO} ngo
  * @param {?Boolean} main use the main photo
  * @param {?Boolean} header use the header photo
- * @param {?Number} imgIdx use a specific image from the image list
+ * @param {Number} imgIdx use a specific image from the image list
  * @param {?Boolean} bg render as a BG component instead
- * @param {?String} src if no NGO is set, will render this like a normal image as a fallback
+ * @param {?String} src if no NGO is set, will render this like a normal image as a fallback (defaults to main photo)
+ * @param {?Boolean} noFallback return null instead of falling back
  */
-const NGOImage = ({ngo, main, header, imgIdx, bg, src, ...props}) => {
+const NGOImage = ({ngo, main, header, imgIdx, bg, src, noFallback, children, ...props}) => {
 
-    assert(ngo || props.src);
+    assert(ngo || src);
+    assert(imgIdx !== undefined || main || header); // temporary
     
+    /*
+
     const [imageIndex, setImageIndex] = useState(-1);
     const [imgUrl, setImgUrl] = useState();
+    const [myID, setMyID] = useState(null);
 
     const NGO_IMG_PATH = ["widget", "NGOImage", ngo.id, "displayed"];
 
+    const domRef = useRef(null);
+
+    const calcPos = () => { return {
+        top: domRef.current && domRef.current.getBoundingRect().top + document.documentElement.scrollTop,
+        left: domRef.current && domRef.current.getBoundingRect().left + document.documentElement.scrollLeft
+    }};
+
     useEffect(() => {
-        let imagesOnPage = DataStore.getValue(NGO_IMG_PATH);
-        if (!imagesOnPage) {
-            DataStore.setValue(NGO_IMG_PATH, []);
-            imagesOnPage = [];
+        let imagesOnPage = DataStore.getValue(NGO_IMG_PATH) || {};
+        let id = myId;
+        if (!id) {
+            id = Date.now();
+            setMyID(id);
         }
-        const idx = imagesOnPage.length;
-        if (idx > ngo.imageList.length - 1) {
-            console.warn("There aren't enough NGO images to fill this page!");
-            return;
-        }
-        setImageIndex(idx);
-        const url = ngo.imageList[idx].contentUrl;
-        setImgUrl(url);
-        imagesOnPage.push(url);
-        console.log("NGOImage set up with index ", idx, url);
-    }, [ngo, main, header, imgIdx]);
+        imagesOnPage[id] = calcPos();
+        DataStore.setValue(NGO_IMG_PATH, imagesOnPage);
+    }, [domRef.current, calcPos()]);
 
-    const useUrl = imgUrl || src;
+    const imagesOnPage = DataStore.getValue(NGO_IMG_PATH) || {};
+    if (myID && !imgIdx) {
+        let myRank = Object.keys(imagesOnPage).length;
+        let myPos = calcPos();
+        Object.keys(imagesOnPage).forEach(id => {
+            if (id !== myID) {
+                if (imagesOnPage[id].top < myPos.top) {
+                    myRank --;
+                } else if (imagesOnPage[id].top === myPos.top) {
+                    if (imagesOnPage[id].left < myPos.left) {
+                        myRank --;
+                    }
+                }
+            }
+        });
+        if (imageIndex !== myRank) setImageIndex(myRank);
+        console.log("NGOImage ID", myID, "ranked", myRank);
+    }*/
 
-    return bg ? <BG src={useUrl} {...props}/>
-        : <img src={useUrl} {...props} />;
+
+    let idx = imgIdx;//|| imageIndex;
+
+    let useUrl = null;
+
+    if (main && ngo.images) useUrl = ngo.images;
+    else if (header && ngo.headerImage) useUrl = ngo.headerImage;
+    else if (!ngo.imageList || idx > ngo.imageList.length - 1 || idx === undefined) {
+        if (noFallback) return null;
+        useUrl = src || ngo.images;
+    } else {
+        useUrl = ngo.imageList[idx].contentUrl;
+    }
+
+    const ImgType = bg ? BG : "img";
+
+    if (children && !bg) {
+        console.warn("NGOImage set to normal image but given children - will not correctly render!");
+    }
+
+    return <ImgType src={useUrl} {...props}>{children}</ImgType>;
 
 };
 
