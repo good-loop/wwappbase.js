db7150da4c577e42b1191232d3945c877bcb679a
Wing Wong <wing@good-loop.com>
2022-04-27 16:35:03 +0100

    sync

--- base/components/PropControl.jsx
+++ base/components/PropControl.jsx
@@ -139,7 +139,7 @@ or if extras like help and error text are wanted.
  * NB: This function provides a label / help / error wrapper -- then passes to PropControl2
  */
 const PropControl = ({className, ...props}) => {
-	let { type = "text", optional, required, path, prop, label, help, tooltip, error, warning, validator, inline, dflt, fast, size, ...stuff } = props;
+	let { type = "text", optional, required, path, prop, label, help, tooltip, error, warning, validator, inline, dflt, fast, size, labelStyle, ...stuff } = props;
 	if ( ! path) {	// default to using path = the url
 		path = ['location', 'params'];
 		props = Object.assign({ path }, props);
@@ -255,7 +255,7 @@ const PropControl = ({className, ...props}) => {
 	return (
 		<FormGroup check={isCheck} className={space(type, className, inline && ! isCheck && 'form-inline', error&&'has-error')} size={size} >
 			{(label || tooltip) && !isCheck &&
-				<label className={space(sizeClass,'mr-1')} htmlFor={stuff.name}>{labelText} {helpIcon} {optreq}</label>}
+				<label className={space(sizeClass,'mr-1')} htmlFor={stuff.name} style={labelStyle}>{labelText} {helpIcon} {optreq}</label>}
 			{inline && ' '}
 			{help && !inline && !isCheck && <Help>{help}</Help>}
 			<PropControl2 storeValue={storeValue} value={value} rawValue={rawValue} setRawValue={setRawValue} proppath={proppath} {...props} pvalue={pvalue} />
