f27bfbc88423b3a2074e631142cb30583653666f
Roscoe <roscoe@good-loop.com>
2022-06-27 18:34:33 +0100

    Logo.jsx: dummy out SVG width-100 rule for now & cleanup

--- base/components/Logo.jsx
+++ base/components/Logo.jsx
@@ -3,33 +3,42 @@ import React from 'react';
 import { space, ellipsize } from '../utils/miscutils';
 import DataClass, {getName} from '../data/DataClass';
 import { assert } from '../utils/assert';
+
+
+// Check for SVG and use specific width if so
+// TODO This breaks layout in the Green Dashboard context where this component is currently used.
+// See CharityLogo.jsx in my-loop for original usage - work out a cleaner solution to the problem this was solving before replacing that
+const svgClass = (logoUrl) => {
+	if (logoUrl?.match(/\.svg(\?.*|#.*)*$/gi)) { // must end with ".svg", allow #... or ?... after
+		return 'w-100'; // width 100?? won't that make it giant in the wrong setting??
+	}
+
+	return null;
+};
+
 /**
  * Logo from branding (also handles NGOs)
  * @param {Object} p
  * @param {DataClass} p.item
  */
-const Logo = ({item, className, size, style, nameCap=24}) => {
-	if (! item) return null;
-    assert(item.id, "Not a DataItem", item);
-    // get branding
-    let branding = item.branding || item; // HACK: NGOs have .logo on the item
-    let altText = item.displayName || item.name || item.id;
-    if (nameCap) altText = ellipsize(altText, nameCap);
-    if ( ! branding.logo) {            
-        return <span className={space(className, "logo", size&&"logo-"+size)} style={style}>{altText}</span>; // fallback to their name
-    }
-    // Check for SVG and use specific width if so
-	let svgClasses="";
-	let imgType = /^.*\.([a-zA-Z]*).*$/g.exec(branding.logo);
-	if (imgType) {
-		imgType = imgType[1];
-		if (imgType.toLowerCase() === "svg") {
-			svgClasses = "w-100"; // width 100?? won't that make it giant in the wrong setting??
-		}
+const Logo = ({item, className, size, style, nameCap = 24}) => {
+	if (!item) return null;
+	assert(item.id, 'Not a DataItem', item);
+
+	// get branding
+	let branding = item.branding || item; // HACK: NGOs have .logo on the item
+	let altText = item.displayName || item.name || item.id;
+	if (nameCap) altText = ellipsize(altText, nameCap);
+
+	const classes = space('logo', size && `logo-${size}`, className, /*svgClass(branding.logo)*/);
+
+	// fallback to entity name
+	if (!branding.logo) {
+		return <span className={classes} style={style}>{altText}</span>;
 	}
-	// 'logo' class forces the logos to be too small for the circle - so leaving it out	
-    return <img className={space(className, "logo", size&&"logo-"+size, svgClasses)} 
-        style={style} src={branding.logo} alt={altText} />;
+
+	// 'logo' class forces the logos to be too small for the circle - so leaving it out
+	return <img className={classes} style={style} src={branding.logo} alt={`Logo for ${altText}`} title={altText} />;
 };
 
 export default Logo;
