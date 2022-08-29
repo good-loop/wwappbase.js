2f7ef53001d3d0c753dd22fe15397fb97df9172f
Daniel Winterstein <daniel@good-loop.com>
2022-08-29 17:43:50 +0100

    doc

--- base/components/MailTo.jsx
+++ base/components/MailTo.jsx
@@ -4,6 +4,8 @@ import LinkOut from './LinkOut';
 
 /**
  * A mailto tag for emails you can click on
+ * 
+ * TODO support for setting email subject
  */
 const MailTo = ({email, children}) => {
     if ( ! email) return children;
