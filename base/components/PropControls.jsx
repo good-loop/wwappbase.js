acea97da8cc70b6ca2d02418bc7e8f2a1f5c9958
Daniel Winterstein <daniel@good-loop.com>
2022-10-29 17:58:12 +0100

    refactor-RC

--- base/components/PropControls.jsx
+++ base/components/PropControls.jsx
@@ -13,8 +13,8 @@ import PropControlSelection from './PropControlSelection';
 import PropControlDataItem from './PropControlDataItem';
 import PropControlMoney from './PropControlMoney';
 import PropControlDt from './PropControls/PropControlDt';
-import PropControlEgo from './PropControls/PropControlEgo';
-import PropControlCode from './PropControls/PropControlCode';
+// import PropControlEgo from './PropControls/PropControlEgo';
+// import PropControlCode from './PropControls/PropControlCode'; this pulls in prism.js as a dependency, so not included in all projects
 import PropControlToggle from './PropControls/PropControlToggle';
 
 import PropControlImg from './PropControls/PropControlImg';
