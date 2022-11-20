998c6439f9591e1c8a0ea32296535f3461002867
Daniel Winterstein <daniel@good-loop.com>
2022-11-20 21:02:44 +0000

    RC

---
+++ base/components/PropControls/PropControlMediaObject.jsx
@@ -0,0 +1,30 @@
+import React, { useEffect, useState } from 'react';
+import { Input, InputGroup } from 'reactstrap';
+import { countryListAlpha2 } from '../../data/CountryRegion';
+
+import PropControl, { fakeEvent, registerControl } from '../PropControl';
+import PropControlSelection from '../PropControlSelection';
+import SubCard from '../SubCard';
+import PropControlPerson from './PropControlPerson';
+const dummy = PropControlPerson;
+
+const PropControlMediaObject = ({path, prop, proppath, uploadType, storeValue, onChange, warnOnUnpublished}) => {
+	return (<>
+        <PropControl type={uploadType} prop='contentUrl' path={proppath} />	
+        <PropControl type="text" prop='caption' path={proppath} label size='sm' />	
+	</>);
+};
+
+/**
+ * See ImageObject.java
+ */
+const PropControlImageObject = (props) => <PropControlMediaObject uploadType='imgUpload' {...props} />;
+/**
+ * See VideoObject.java
+ */
+ const PropControlVideoObject = (props) => <PropControlMediaObject uploadType='videoUpload' {...props} />;
+
+registerControl({type: 'ImageObject', $Widget: PropControlImageObject});
+registerControl({type: 'VideoObject', $Widget: PropControlVideoObject});
+
+export default {};
