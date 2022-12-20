75a0c66dd5b7610badb71830eb0a3fc5d947ea47
Daniel Winterstein <daniel@good-loop.com>
2022-12-20 15:30:38 +0000

    ImpactDebit parent

--- base/components/PortalLink.jsx
+++ base/components/PortalLink.jsx
@@ -3,8 +3,14 @@ import ServerIO from '../plumbing/ServerIOBase';
 import C from '../CBase';
 import { getId, getType } from '../data/DataClass';
 import { encURI, space } from '../utils/miscutils';
+import Roles from '../Roles';
 
 
+/**
+ * An internal link
+ * @param {Object} p
+ * @param {?boolean} devOnly
+ */
 const PortalLink = ({item,size,className,devOnly}) => {
     if (devOnly && ! Roles.isDev()) return null;
     let url = ServerIO.getEndpointForType(getType(item));
