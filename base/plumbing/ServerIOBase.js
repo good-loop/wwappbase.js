0ffe468e68897d7b561e6bd53f8afb0d85127a33
Daniel Winterstein <daniel@good-loop.com>
2023-01-05 18:07:26 +0000

    access=public loophole

--- base/plumbing/ServerIOBase.js
+++ base/plumbing/ServerIOBase.js
@@ -149,11 +149,12 @@ const checkBase2_toggleTestEndpoints = () => {
 		return;
 	}
 	if (server === 'local') { // probably not needed
-		ServerIO.AS_ENDPOINT = 'http://localas.good-loop.com';
-		ServerIO.PORTAL_ENDPOINT = 'http://localportal.good-loop.com';
-		ServerIO.DATALOG_ENDPOINT = 'http://locallg.good-loop.com/data';
-		ServerIO.PROFILER_ENDPOINT = 'http://localprofiler.good-loop.com';
-		ServerIO.MEDIA_ENDPOINT = 'http://localuploads.good-loop.com';
+		const protocol = window.location.protocol;
+		ServerIO.AS_ENDPOINT = protocol+'//localas.good-loop.com';
+		ServerIO.PORTAL_ENDPOINT = protocol+'//localportal.good-loop.com';
+		ServerIO.DATALOG_ENDPOINT = protocol+'//locallg.good-loop.com/data';
+		ServerIO.PROFILER_ENDPOINT = protocol+'//localprofiler.good-loop.com';
+		ServerIO.MEDIA_ENDPOINT = protocol+'//localuploads.good-loop.com';
 		ServerIO.APIBASE = ''; // lets assume you're on local
 		return;
 	}
