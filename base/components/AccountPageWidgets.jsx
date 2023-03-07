f20e2849495da4c22a02e53d18d23b40b7e710db
Wing <wing@good-loop.com>
2023-03-07 17:31:35 +0000

    account page status card

--- base/components/AccountPageWidgets.jsx
+++ base/components/AccountPageWidgets.jsx
@@ -10,24 +10,30 @@ import Misc from './Misc';
 import CardAccordion from './CardAccordion'; // Hack: this is here to poke CardAccordion into Misc for older code
 import PropControl from './PropControl'; // Hack: this is here to poke Input into Misc for older code
 import XId from '../data/XId';
-import {LoginLink} from './LoginWidget';
-import {setTaskTags} from './TaskList';
+import { LoginLink } from './LoginWidget';
+import { setTaskTags } from './TaskList';
 import AboutPage from './AboutPage';
-import { Card } from 'reactstrap';
+import { Button, Card } from 'reactstrap';
 import ShareWidget, { ShareLink } from './ShareWidget';
 
 const BasicAccountPage = () => {
 	if (!Login.isLoggedIn()) {
-		return <div><h1>My Account: Please login</h1><LoginLink title="Login" /></div>;
+		return (
+			<div>
+				<h1>My Account: Please login</h1>
+				<LoginLink title='Login' />
+			</div>
+		);
 	}
-	
+
 	setTaskTags();
 	return (
 		<div className=''>
 			<h1>My Account</h1>
 			<LoginCard />
 			<RolesCard />
-			
+			<StatusCard />
+
 			{/* minor todo <Card>
 				<AboutPage />
 			</Card> */}
@@ -36,31 +42,67 @@ const BasicAccountPage = () => {
 };
 
 const LoginCard = () => {
-	return (<Misc.Card title="Login">
-		ID: {Login.getId()} <br />
-	</Misc.Card>);
-}
+	return (
+		<Misc.Card title='Login'>
+			ID: {Login.getId()} <br />
+		</Misc.Card>
+	);
+};
 
 const RolesCard = () => {
-	let proles =Roles.getRoles();
+	let proles = Roles.getRoles();
 	let roles = proles.value;
 
-	return (<Misc.Card title="Roles">
-		<p>Roles determine what you can do. E.g. only editors can publish changes.</p>
-		{roles? roles.map((role, i) => <RoleLine key={i+role} role={role} />) : <Misc.Loading />}		
-		{roles && roles.includes("admin") && C.ROLES && <div><hr/>{C.ROLES.values.map((role, i) => <RoleLine key={"admin"+i+role} role={role} />)}</div>}
-	</Misc.Card>);
-}
-
-
-const RoleLine = ({role}) => {
-	return <div className="badge badge-pill badge-info">{role}
-		{(Roles.isDev() || Roles.iCan("admin")) && <><ShareLink shareId={"role:"+role}/><ShareWidget key={role} shareId={"role:"+role} /></>}
-	</div>;
-}
-
-export {
-	BasicAccountPage,
-	RolesCard,
-	LoginCard
+	return (
+		<Misc.Card title='Roles'>
+			<p>Roles determine what you can do. E.g. only editors can publish changes.</p>
+			{roles ? roles.map((role, i) => <RoleLine key={i + role} role={role} />) : <Misc.Loading />}
+			{roles && roles.includes('admin') && C.ROLES && (
+				<div>
+					<hr />
+					{C.ROLES.values.map((role, i) => (
+						<RoleLine key={'admin' + i + role} role={role} />
+					))}
+				</div>
+			)}
+		</Misc.Card>
+	);
 };
+
+const RoleLine = ({ role }) => {
+	return (
+		<div className='badge badge-pill badge-info'>
+			{role}
+			{(Roles.isDev() || Roles.iCan('admin')) && (
+				<>
+					<ShareLink shareId={'role:' + role} />
+					<ShareWidget key={role} shareId={'role:' + role} />
+				</>
+			)}
+		</div>
+	);
+};
+
+const StatusCard = () => {
+	const doSendVerifyEmail = (e) => {
+		e.preventDefault();
+		let email = Login.getEmail();
+		assMatch(email, String);
+		let call = Login.sendVerify(email)
+			.then((res) => {
+				console.log(res);
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	};
+
+	return (
+		<Misc.Card title='Verification'>
+			Verification Status: {Login.getStatus()} <br />
+			<Button onClick={doSendVerifyEmail}>doSendVerifyEmail</Button>
+		</Misc.Card>
+	);
+};
+
+export { BasicAccountPage, RolesCard, LoginCard, StatusCard };
