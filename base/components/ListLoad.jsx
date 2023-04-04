b78e98267ed78f7eb9291c1bd60d8bd75da02d71
Daniel Winterstein <daniel@good-loop.com>
2023-04-04 12:46:21 +0100

    ImpactCredit-work

--- base/components/ListLoad.jsx
+++ base/components/ListLoad.jsx
@@ -54,7 +54,7 @@ import Roles from '../Roles';
  * @param {?Function} p.ListItem JSX if set, replaces DefaultListItem.
  * 	ListItem only has to describe/present the item.   
  * 	NB: On-click handling, checkboxes and delete are provided by ListItemWrapper.   
- * 	Input props: {type, servlet, navpage, item, sort}
+ * 	Input props: {type, servlet, navpage, item, sort, items, nameFn, onClick}
  * @param {?Function} p.nameFn passed to ListItem, to have custom name extraction
  * @param {?boolean} p.notALink - (Deprecated - see cannotClick) Normally list items are a-tag links. If true, use div+onClick instead of a, so that the item can hold a tags (which don't nest).* 
  * @param {?String} p.itemClassName - If set, overrides the standard ListItem btn css classes
@@ -229,6 +229,7 @@ function ListLoad({ type, status, servlet, navpage,
 					navpage={navpage}
 					item={item}
 					sort={DataStore.getValue(['misc', 'sort'])}
+					items={allItems} /* dont paginate */
 					nameFn={nameFn}
 					onClick={() => onClickItem(item)}
 				/>
