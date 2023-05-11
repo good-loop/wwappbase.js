bea70e553c1651503ea42e28239410ccd96b9d01
dodo721 <dodoworkspr@gmail.com>
2023-05-11 12:54:23 +0100

    Moved legacy code into its own file

--- base/data/Campaign.js
+++ base/data/Campaign.js
@@ -1,4 +1,4 @@
-/** Data model functions for the Advert data-type. */
+/** Data model functions for the Campaign data-type. */
 import { assert, assMatch } from '../utils/assert';
 import Enum from 'easy-enums';
 import DataClass from './DataClass';
@@ -95,21 +95,6 @@ Campaign.start = item => {
 	let tli = item.topLineItem;
 	return tli? asDate(tli.end) : null;
 }
-Campaign.isOngoing = campaign => {
-	let end = Campaign.end(campaign);
-	if (end) {
-		return end.getTime() > new Date().getTime();
-	}
-	// old data?
-	return campaign.ongoing;
-};
-
-/**
- * @deprecated only retained so we can detect and filter out old master campaigns
- * @param {!Campaign} campaign 
- * @returns {boolean} NB: false for the TOTAL_IMPACT root 
- */
-Campaign.isMaster = campaign => Campaign.assIsa(campaign) && campaign.master;
 
  /** 
  * @param {Advert} advert 
@@ -215,117 +200,6 @@ const getImpactDebits2 = async (campaign, status) => {
 }
 
 /**
- * 
- * @param {!Campaign} campaign 
- * @returns {?String[]} ids to hide
- */
-Campaign.hideCharities = campaign => {
-	Campaign.assIsa(campaign);
-	let hc = campaign.hideCharities;
-	if ( ! hc) return null;
-
-	// hideCharities is from a KeySet prop control, so is an object of schema {charity_id : bool}.
-	// We want to convert it instead to a list of charity IDs
-	if (Array.isArray(hc)) {
-		return hc;
-	}
-	// Convert object to array
-	let hideCharitiesArr = Object.keys(hc);
-	// Remove false entries - keySet will not remove charity IDs, but set them to false instead.
-	hideCharitiesArr = hideCharitiesArr.filter(cid => hc[cid]);
-	return hideCharitiesArr;
-};
-
-/**
- * Get the list of ad IDs that this campaign will hide
- * NB: While you can merge the hideAds list with other campaigns, this is not used within the Impact Hub,
- * for simplicity of only having one list to manage instead of multiple across multiple campaigns
- * @param {Campaign} topCampaign 
- * @param {?Campaign[]} campaigns other campaigns to merge with
- * @returns {!String[]} hideAdverts IDs
- */
-Campaign.hideAdverts = (topCampaign, campaigns) => {
-    Campaign.assIsa(topCampaign);
-    // Merge all hide advert lists together from all campaigns
-    let allHideAds = topCampaign.hideAdverts ? keysetObjToArray(topCampaign.hideAdverts) : [];
-    if (campaigns) {
-		campaigns.forEach(campaign => allHideAds.push(... campaign.hideAdverts ? keysetObjToArray(campaign.hideAdverts) : []));
-	}
-    // Copy array
-    const mergedHideAds = allHideAds.slice();
-    return mergedHideAds;
-}
-
-
-/**
- * Get (and cache) all ads associated with the given campaign. This will apply hide-list and never-served filters 
-* @param {Object} p 
- * @param {Campaign} p.campaign 
- * @param {?KStatus} p.status
- * @param {?String} p.query Filter by whatever you want, eg data
- * @returns PromiseValue(List(Advert)) HACK Adverts get `_hidden` added if they're excluded.
- */
-Campaign.pvAds = ({campaign,status=KStatus.DRAFT,query}) => {
-	let pv = DataStore.fetch(['misc','pvAds',status,query||'all',campaign.id], () => {
-		return pAds2({campaign,status,query});
-	});
-	return pv;
-};
-
-/** Newest first. Safe default for pretty much everywhere here. */
-const sortAdsList = adsList => adsList.hits.sort((a, b) => {
-	if (a.created === b.created) return 0;
-	return a.created < b.created ? 1 : -1;
-});
-
-/**
- * HACK: access=public
- * NB: This function does chained promises, so we use async + await for convenience.
- * @returns Promise List(Advert) All ads -- hidden ones are marked with a truthy `_hidden` prop
- */
-const pAds2 = async function({campaign, status, query, isSub}) {
-	Campaign.assIsa(campaign);
-	// fetch ads
-	let sq = SearchQuery.setProp(null, "campaign", campaign.id);
-	if (query) sq = SearchQuery.and(sq, new SearchQuery(query));
-	// ...HACK allow Impact Hub to fetch an unfiltered but cleansed list
-	//    But not for previews, as access=public cannot read DRAFT
-	const access = status==KStatus.PUBLISHED? "public" : null; 
-	// ...fetch
-	const pvAds = ActionMan.list({type: C.TYPES.Advert, status, q:sq.query, access});
-	let adl = await pvAds.promise;
-	List.assIsa(adl);
-	sortAdsList(adl);
-
-	// Label ads using hide list and non-served
-	let ads = List.hits(adl);
-	pAds3_labelHidden({campaign, ads});
-
-	return adl;
-};
-
-const pAds3_labelHidden = ({campaign, ads}) => {
-	// manually hidden
-	const hideAdverts = Campaign.hideAdverts(campaign);
-	for (let hi = 0; hi < hideAdverts.length; hi++) {
-		const hadid = hideAdverts[hi];
-		const ad = ads.find(ad => ad.id === hadid);
-		if (ad) {
-			ad._hidden = campaign.id; // truthy + tracks why it's hidden
-		}
-	}
-	// non served
-	if (campaign.showNonServed) {
-		return;
-	}
-	ads.forEach(ad => {
-		if ( ! ad.hasServed && ! ad.serving) {
-			ad._hidden = "non-served";
-		}
-	});
-};
-
-/**
  * Get the viewcount for a campaign, summing the ads (or using the override numPeople)
  * @param {Object} p
  * @param {Campaign} p.campaign 
@@ -336,193 +210,12 @@ Campaign.viewcount = ({campaign, status}) => {
 	if (campaign.numPeople) {
 		return campaign.numPeople;
 	}
-	const pvAllAds = Campaign.pvAds({campaign, status});
+	const pvAllAds = Advert.fetchForCampaign({campaignId:campaign.id, status});
 	let allAds = List.hits(pvAllAds.value) || [];
 	const viewcount4campaign = Advert.viewcountByCampaign(allAds);
 	let totalViewCount = sum(Object.values(viewcount4campaign));
 	return totalViewCount;
 };
-
-
-////////////////////////////////////////////////////////////////////
-////                     CHARITY LOGIC                           ///
-////////////////////////////////////////////////////////////////////
-
-/**
-<<<<<<<<< Temporary merge branch 1
-=========
- * 
- * @param {!Campaign} campaign 
- * @returns {!Object} {type, id} a stub object for the master agency/advertiser, or {}. 
- * NB: advertiser takes precedence, so you can usefully call this on a leaf campaign.
- * NB: {} is to support `let {type, id} = Campaign.masterFor()` without an NPE
- */
-Campaign.masterFor = campaign => {
-	Campaign.assIsa(campaign);
-	if (campaign.vertiser) return {type:C.TYPES.Advertiser, id:campaign.vertiser};
-	if (campaign.agencyId) return {type:C.TYPES.Agency, id:campaign.agencyId};
-	return {};
-};
-
-/**
- * * HACK: access=public
- * @param {Object} obj
- * @param {!Campaign} obj.campaign 
- * @param {?SearchQuery | string} obj.query
- * @returns PV(List<Campaign>) Includes campaign! Beware when recursing
- */
-Campaign.pvSubCampaigns = ({campaign, query}) => {
-	Campaign.assIsa(campaign);
-	if ( ! campaign.master) {
-		return new PromiseValue(new List([campaign]));
-	}
-	// fetch leaf campaigns	
-	let {id, type} = Campaign.masterFor(campaign);
-	// campaigns for this advertiser / agency
-	let sq = SearchQuery.setProp(query, C.TYPES.isAdvertiser(type)? "vertiser" : "agencyId", id);
-	// exclude this? No: some (poorly configured) master campaigns are also leaves
-	// sq = SearchQuery.and(sq, "-id:"+campaign.id); 	
-	const access = "public"; // HACK allow Impact Hub to fetch an unfiltered list
-	const pvCampaigns = getDataList({type: C.TYPES.Campaign, status:KStatus.PUBLISHED, q:sq.query, access}); 
-	// NB: why change sub-status? We return the state after this campaign is published (which would not publish sub-campaigns)
-	return pvCampaigns;
-};
-
-
-/** 
- * @deprecated
- * This is the total unlocked across all adverts in this campaign. See also maxDntn.
- * Warning: This will change as data loads!!
- * @returns {?Money}
- */
- Campaign.dntn = (campaign, isSub) => {
-	if ( ! campaign) return null;
-	Campaign.assIsa(campaign);
-	if (campaign.dntn) {
-		// HACK realtime=true forces a realtime fetch
-		if ( ! getUrlVars().realtime) {
-			return campaign.dntn;
-		}
-	}
-	if ( ! campaign.master || isSub) {
-		// Ask the backend
-		let sq = SearchQuery.setProp(null, "campaign", campaign.id);
-		let pvDntnData = DataStore.fetch(['misc','donations',campaign], 
-			() => ServerIO.getDonationsData({t:'dntnblock', q:sq.query, name:"campaign-donations"}), 
-			{cachePeriod:300*1000});
-		let total = pvDntnData.value && pvDntnData.value.total;
-		return total;
-	}
-	// recurse
-	// NB: Wouldn't it be faster to do a one-batch data request? Yeah, but that would lose the Campaign.dntn hard-coded info.
-	// TODO: make it so datalog reconciles with that, so we can do batched requests
-	let pvSubs = Campaign.pvSubCampaigns({campaign});
-	if ( ! pvSubs.value) {
-		return null;
-	}
-	let subs = List.hits(pvSubs.value);
-	let dntns = subs.map(sub => Campaign.dntn(sub, true));
-	let total = Money.total(dntns);
-	return total;
-};
-
-
-/**
- * Recursive and fetches dynamic data.
- * @deprecated
- 
- * @param {!Campaign} campaign 
-*  @param {?boolean} isSub set in recursive calls
- * @returns {!Object} {cid: Money} Values may change as data loads
- * @see Campaign.dntn
- */
-Campaign.dntn4charity = (campaign, isSub) => {
-	assert( ! isSub || isSub===true, isSub);
-	Campaign.assIsa(campaign);
-	if ( ! campaign.master || isSub) {
-		// leaf
-		// hard set values?
-		let d4c = Object.assign({}, campaign.dntn4charity); // defensive copy, never null
-		// HACK realtime=true forces a realtime fetch
-		if (getUrlVars().realtime) {
-			d4c = {};
-		}
-		// are any missing?
-		let charities = Campaign.charities(campaign);
-		let missingNGOs = charities.filter(ngo => ! d4c[NGO.id(ngo)]);
-		if ( ! missingNGOs.length) {
-			return d4c;
-		}
-		// HACK realtime off??
-		if (getUrlVars().realtime === false) {
-			return d4c
-		}
-		// Ask the backend
-		let sq = SearchQuery.setProp(null, "campaign", campaign.id);
-		let pvDntnData = DataStore.fetch(['misc','donations',campaign], 
-			() => ServerIO.getDonationsData({q:sq.query, name:"campaign-donations"}), 
-			{cachePeriod:300*1000});
-		if ( ! pvDntnData.value) {
-			return d4c;
-		}		
-		let by_cid = pvDntnData.value.by_cid;
-		// merge with top-level
-		let alld4c = Object.assign({}, by_cid, d4c);
-		return alld4c;
-	} // ./leaf
-	// Master - recurse - sum leaf campaigns
-	if ( ! isDntn4CharityEmpty(campaign.dntn4charity)) {
-		console.warn("Ignoring master.dntn4charity - it should not be set for masters 'cos it can confuse sums for e.g. reporting by-charity in T4G", campaign);
-	}
-	let pvSubs = Campaign.pvSubCampaigns({campaign});
-	if ( ! pvSubs.value) {
-		return {};
-	}
-	let subs = List.hits(pvSubs.value);
-	let dntn4charitys = subs.map(sub => Campaign.dntn4charity(sub, true));
-	// merge + sum subs
-	let subtotal4c = {};
-	for(let i=0; i<dntn4charitys.length; i++) {
-		const subd4c = dntn4charitys[i];
-		mapkv(subd4c, (k,v) => {
-			let old = subtotal4c[k];
-			subtotal4c[k] = old? Money.add(old, v) : v;
-		});
-	}
-	return subtotal4c;
-};
-
-
-
-
-/**
->>>>>>>>> Temporary merge branch 2
- * FIXME Get a list of charities for a campaign
- * @param {Object} p 
- * @param {Campaign} p.campaign 
- * @param {KStatus} p.status The status of ads and sub-campaigns to fetch
- * @returns {NGO[]} May change over time as things load!
- */
- Campaign.charities = (campaign, status=KStatus.DRAFT, isSub) => {
-	Campaign.assIsa(campaign);
-	KStatus.assert(status);
-	// charities listed here
-	let charityIds = [];
-	if (campaign.strayCharities) charityIds.push(...campaign.strayCharities);
-	if (campaign.dntn4charity) charityIds.push(...Object.keys(campaign.dntn4charity));
-	if (campaign.localCharities) charityIds.push(...Object.keys(campaign.localCharities));	
-
-	let pvAds = Campaign.pvAds({campaign, status});
-	if ( ! pvAds.value) {
-		return charities2(campaign, charityIds, []);
-	}
-	let ads = List.hits(pvAds.value);
-	if ( ! ads.length) console.warn("No Ads?!", campaign, status);
-	// individual charity data, attaching ad ID
-	let vcharitiesFromAds = charities2_fromAds(ads);
-	// apply local edits
-	return charities2(campaign, charityIds, vcharitiesFromAds);
-}; // ./charities()
 
 /**
  * 
@@ -536,7 +229,7 @@ Campaign.viewcountByCountry = ({campaign, status}) => {
 		console.log("res: no camp!")
 		return []
 	}
-	const pvAllAds = Campaign.pvAds({campaign, status});
+	const pvAllAds = Advert.fetchForCampaign({campaignId:campaign.id, status});
 	let allAds = List.hits(pvAllAds.value) || [];
 	console.log("res allAds: ", allAds)
 	const viewcount4campaign = Advert.viewcountByCountry({ads:allAds});
