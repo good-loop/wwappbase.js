c60a696e6f963d31f6303ee8d6d216a3e66a4a42
Roscoe <roscoe@good-loop.com>
2023-06-14 21:23:11 +0100

    Fix some bugs in ListLoad pagination and pull out pager component for reuse

--- base/components/ListLoad.jsx
+++ base/components/ListLoad.jsx
@@ -22,6 +22,8 @@ import List from '../data/List';
 import { A, modifyPage } from '../plumbing/glrouter';
 import Roles from '../Roles';
 
+import Pager from './Pager';
+
 const DEFAULT_PAGE_SIZE = 100;
 
 /**
@@ -55,7 +57,7 @@ const DEFAULT_PAGE_SIZE = 100;
  * Best practice is to set navpage to avoid relying on url behaviour.
  * @param {Function} [p.ListItem] JSX if set, replaces DefaultListItem.
  * 	ListItem only has to describe/present the item.   
- * 	NB: On-click handling, checkboxes and delete are provided by ListItemWrapper.   
+ * 	NB: On-click handling, checkboxes and delete are provided by ListItemWrapper.
  * 	Input props: {type, servlet, navpage, item, sort, items, nameFn, onClick}
  * @param {Function} [p.nameFn] passed to ListItem, to have custom name extraction
  * @param {boolean} [p.notALink] - (Deprecated - see cannotClick) Normally list items are a-tag links. If true, use div+onClick instead of a, so that the item can hold a tags (which don't nest).* 
@@ -67,9 +69,10 @@ const DEFAULT_PAGE_SIZE = 100;
  * @param {Boolean} [p.unwrapped] If set don't apply a ListItemWrapper (which has the standard on-click behaviour and checkbox etc controls)
  * @param {React.Element|String} [p.noResults]  Message to show if there are no results
  * @param {Function} [p.onClickItem]  Custom non-navigation action when list item clicked
- * @param {Function} [p.onClickWrapper]  Custom non-navigation action when list item wrapper is clicked. Like onClickItem but it applies one level up the dom.
+ * @param {Function} [p.onClickWrapper] Custom non-navigation action when list item wrapper is clicked. Like onClickItem but it applies one level up the dom.
  * @param {Function} [p.pageSelectID] - 1-indexed. If using multiple pages for items this is required to target the specific ListLoad from a url parameter.
 Use-case??
+ * @param {Number} [p.pageSize] Number of items per page, default 100
  * @param {Object} [p.otherParams] Optional extra params to pass to ActionMan.list() and on to the server.
  */
 function ListLoad({ type, status, servlet, navpage,
@@ -211,7 +214,7 @@ function ListLoad({ type, status, servlet, navpage,
 	if (pageSize) items = paginate({ items, page, pageSize });
 
 	// Generate the pagination links, if applicable
-	const pageControls = <PageControls setPage={setPage} current={page} pageCount={pageCount} />;
+	const pageControls = <Pager setPage={setPage} current={page} pageCount={pageCount} />;
 
 	// Static props common to all ListItemWrappers & ListItems
 	const wrapperCommon = { type, servlet, navpage, unwrapped, checkboxes, canCopy, cannotClick, canDelete, notALink, itemClassName, onClickWrapper };
@@ -240,122 +243,6 @@ function ListLoad({ type, status, servlet, navpage,
 		<ErrAlert error={error} />
 	</div>);
 } // ./ListLoad
-
-
-/**
- * Generates a Reactstrap <Pagination> for the current ListLoad.
- *
- * @param {Object} props All passed down to PageBtn
- */
-function PageControls(props) {
-	const { pageCount, current, setPage } = props;
-	if (!pageCount || pageCount <= 1) return null;
-
-	const [pagerEl, setPagerEl] = useState();
-	const [items, setItems] = useState([]);
-	const [showButtonCount, setShowButtonCount] = useState(null); // How many buttons do we have room for?
-	const [measured, setMeasured] = useState(false); // Has the pager been rendered with a properly set button count?
-
-	// Fit item count to available space
-	useEffect(() => {
-		// Previously measured but container width changed - invalidate size calibration.
-		if (measured) {
-			setMeasured(false);
-			setShowButtonCount(null);
-			return;
-		}
-		if (!pagerEl) return;
-
-		// How wide - on average - are the pager buttons?
-		const itemEls = pagerEl.querySelectorAll('li.page-item');
-		let avgSize = 0;
-		itemEls.forEach(el => avgSize += el.clientWidth);
-		avgSize /= itemEls.length;
-
-		// How many non-number buttons are also taking up space?
-		let extraItems = 2; // prev and next
-		if (pageCount > 2) extraItems += 2; // first and last
-
-		// So - this is how many number buttons (including ... skipped-item indicators) there's room for.
-		setShowButtonCount(Math.floor((pagerEl.clientWidth / avgSize) - extraItems));
-	}, [pagerEl?.clientWidth]);
-
-	// Only create list when key props change
-	useEffect(() => {
-		if (!pageCount) return;
-		// Render once, invisible, with all page buttons to calibrate sizing
-		let firstButton = 1, lastButton = pageCount;
-		if (showButtonCount) {
-			firstButton = Math.max(1, Math.ceil(current - (showButtonCount / 2)));
-			lastButton = Math.min(pageCount, Math.floor(current + (showButtonCount / 2)));
-			if (firstButton === 1) lastButton = Math.min(pageCount, showButtonCount);
-			if (lastButton === pageCount) firstButton = Math.max(1, pageCount - showButtonCount);
-		}
-
-		const nextItems = [];
-		if (pageCount > 2) nextItems.push(<PageBtn key="first" first {...props} />);
-		nextItems.push(<PageBtn key="prev" previous {...props} />);
-		for (let i = firstButton; i <= lastButton; i++) {
-			// Dummy "..." buttons still take up space - so they come out of the button allowance.
-			if (i === firstButton && firstButton > 1) {
-				nextItems.push(<PageBtn key="skip-low" dummy />);
-			} else if (i === lastButton && lastButton < pageCount) {
-				nextItems.push(<PageBtn key="skip-high" dummy />);
-			} else {
-				nextItems.push(<PageBtn key={`page-${i}`} target={i} {...props} />);
-			}
-		}
-		nextItems.push(<PageBtn key="next" next {...props} />);
-		if (pageCount > 2) nextItems.push(<PageBtn key="last" last {...props} />);
-
-		setItems(nextItems);
-		// The rendered item list should be the right length to fit its container & can be made visible.
-		if (showButtonCount) setMeasured(true);
-	}, [pageCount, current, setPage, showButtonCount]);
-
-
-	// Override default <nav> to <div> to play nicely with our CSS.
-	return <div className={space('pager', measured && 'measured')} ref={setPagerEl}>
-		<Pagination>
-			{items}
-		</Pagination>
-	</div>;
-}
-
-
-/**
- * Handles a lot of boilerplate for generating <PaginationItem>s and <PaginationLink>s
- *
- * @param {Object} p
- * @param {!Function} p.setPage On-click for each button. Takes a target page number.
- * @param {!Number} p.target Page the link should go to
- * @param {!Number} p.current Current page number
- * @param {!Number} p.pageCount Total page count
- * @param {!Boolean} p.dummy Not a link, just a "page buttons omitted" ellipsis
- */
-function PageBtn({setPage, target, current, pageCount, dummy, ...linkProps}) {
-	if (dummy) return <PaginationItem disabled><PaginationLink>…</PaginationLink></PaginationItem>;
-
-	const {first, previous, next, last} = linkProps;
-	const itemProps = {};
-	let text = null;
-	if (first || previous) {
-		target = first ? 1 : (current - 1);
-		itemProps.disabled = (current === 1);
-	} else if (next || last) {
-		target = last ? pageCount : (current + 1);
-		itemProps.disabled = (current === pageCount);
-	} else {
-		text = target;
-		itemProps.active = (current === target);
-	}
-
-	return <PaginationItem {...itemProps}>
-		<PaginationLink {...linkProps} onClick={() => setPage(target)}>{text}</PaginationLink>
-	</PaginationItem>;
-}
-
-
 
 
 /**
