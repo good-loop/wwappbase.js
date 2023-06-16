f7bbe5480583ebdbad6e1e738be6bd143ded1579
Daniel Winterstein <daniel@good-loop.com>
2023-06-16 18:56:35 +0100

    impact-certificates-wip

--- base/components/PortalLink.jsx
+++ base/components/PortalLink.jsx
@@ -33,6 +33,8 @@ export const getPortalLink = (item) => {
 		return null;
 	}
 	let url = ServerIO.getEndpointForType(type);
+	// HACK charity - edit in Portal
+	url = url.replace("app.sogive.org/charity","portal.good-loop.com/ngo");
 	url = url.replace("good-loop.com/", "good-loop.com/#"); // hack: switch from servlet to editor page
 	let href = url+"/"+encURI(getId(item));
 	return href;
