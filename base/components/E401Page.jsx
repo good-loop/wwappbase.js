88da35b7cf449660094729662e2c66b5a802c9f7
Daniel Winterstein <daniel@good-loop.com>
2023-06-19 17:22:48 +0100

    e401

---
+++ base/components/E401Page.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const E401Page = () => {
+	return (
+		<div className="E404Page">
+			<h1>Error 401: You're not logged in</h1>
+
+			<p>
+				Sorry: <code>{""+window.location}</code> needs a valid login.
+			</p>
+            <LoginWidgetEmbed verb="login" canRegister={false} />
+		</div>
+	);
+};
+
+export default E401Page;
