39bd91ffb6c278fda0116938e2227feba774b2a6
Roscoe <roscoe@good-loop.com>
2023-06-29 17:29:25 +0100

    PageRecommendations (page/creative optimisation recs utilities) expand to match expanded RecompressServlet

--- base/components/PageRecommendations.jsx
+++ base/components/PageRecommendations.jsx
@@ -13,6 +13,24 @@ import { nonce } from '../data/DataClass';
 const RECS_PATH = ['widget', 'page-recommendations'];
 
 
+function Improvement({origSpec, optSpec}) {
+	const { shortName, bytes: origBytes } = origSpec;
+	const { url: optUrl, bytes: optBytes } = optSpec;
+
+	// How much smaller?
+	const improvement = (1 - (optBytes / origBytes));
+
+	// Don't bother telling user about less than 1% improvement.
+	if (improvement < 0.01) return <div>
+		This resource is already optimally sized.
+	</div>;
+
+	return <div>
+		<a href={optUrl} download={shortName}>Optimised version</a> is {printer.prettyInt(optBytes)} bytes ({(improvement * 100).toFixed(1)}% smaller)
+	</div>;
+}
+
+
 export function FontRecommendation({spec}) {
 	const { shortName, bytes, url, characters } = spec;
 	const [optSpec, setOptSpec] = useState();
@@ -30,25 +48,20 @@ export function FontRecommendation({spec}) {
 		<div className="preview font-preview p-1">
 			<StyleBlock>{`@font-face { font-family: "${previewNonce}"; src: url("${proxy(url)}"); }`}</StyleBlock>
 			<div className="font-preview p-2" style={{fontFamily: previewNonce}}>
-				AaBb12!?
+				{shortName}
 			</div>
 		</div>
 		<CardBody className="p-2">
-			<div>Font {shortName} is {printer.prettyInt(bytes)} bytes.</div>
-			{/* TODO Proxy font to fix CORS issues with preview */}
-			{characters ? <>
-				<div>Characters used in page: <code>{characters}</code></div>
-				{optSpec ? <div>
-					<a href={optSpec.url} download={shortName}>Optimised version</a> is {printer.prettyInt(optSpec.size)} bytes ({((1 - (optSpec.size / bytes)) * 100).toFixed(1)}% smaller)
-				</div> : <Misc.Loading text="Generating optimised font..." />}
-			</>: null}
+			<div>Original font is {printer.prettyInt(bytes)} bytes.</div>
+			{characters ? <div>Restricted character usage found.</div> : null}
+			{optSpec ? <Improvement origSpec={spec} optSpec={optSpec} /> : <Misc.Loading text="Optimising font file..." />}
 		</CardBody>
 	</Card>;
 }
 
 
 export function ImgRecommendation({spec}) {
-	const { shortName, bytes, url } = spec;
+	const { bytes, url } = spec;
 	const [optSpec, setOptSpec] = useState();
 	const noWebp = DataStore.getValue([...RECS_PATH, 'noWebp']) || false;
 
@@ -63,40 +76,74 @@ export function ImgRecommendation({spec}) {
 		<CardHeader>Optimise Image</CardHeader>
 		<img className="preview img-preview p-1" src={url} />
 		<CardBody className="p-2">
-			<div>Image {shortName} is {printer.prettyInt(bytes)} bytes.</div>
+			<div>Original image is {printer.prettyInt(bytes)} bytes.</div>
 			<PropControl type="checkbox" path={RECS_PATH} prop="noWebp" label={`Can't use .webp`} />
-			{optSpec ? <div>
-				<a href={optSpec.url} download={shortName}>Optimised version</a> is {printer.prettyInt(optSpec.size)} bytes ({((1 - (optSpec.size / bytes)) * 100).toFixed(1)}% smaller)
-			</div> : <Misc.Loading />}
+			{optSpec ? <Improvement origSpec={spec} optSpec={optSpec} /> : <Misc.Loading text="Optimising image file..." />}
 		</CardBody>
 	</Card>;
 }
 
 
 export function GifRecommendation({spec}) {
-	const { shortName, bytes, url } = spec;
+	const { bytes, url } = spec;
+	const [optSpec, setOptSpec] = useState();
+
+	useEffect(() => {
+		ServerIO.load(RECOMPRESS_ENDPOINT, { data: { url, type: 'gif' } }).then(res => {
+			setOptSpec({ ...res.data });
+		});
+	}, [url]);
 
 	return <Card className="opt-rec gif-rec">
-		<CardHeader>Optimise FIG</CardHeader>
+		<CardHeader>Optimise GIF</CardHeader>
 		<img className="preview gif-preview p-1" src={url} />
 		<CardBody className="p-2">
-			<div>GIF {shortName} is {printer.prettyInt(bytes)} bytes.</div>
-			<div>TODO recompress</div>
+			<div>Original GIF is {printer.prettyInt(bytes)} bytes.</div>
+			{optSpec ? <Improvement origSpec={spec} optSpec={optSpec} /> : <Misc.Loading text="Converting GIF animation..." />}
 		</CardBody>
 	</Card>;
 }
 
 
 export function SvgRecommendation({spec}) {
-	const { shortName, bytes, url } = spec;
+	const { bytes, url } = spec;
+	const [optSpec, setOptSpec] = useState();
+
+	useEffect(() => {
+		ServerIO.load(RECOMPRESS_ENDPOINT, { data: { url, type: 'svg' } }).then(res => {
+			setOptSpec({ ...res.data });
+		});
+	}, [url]);
 
 	return <Card className="opt-rec svg-rec">
-		<CardHeader>Optimise Inline SVG</CardHeader>
-		<img className="preview font-preview p-1" src={url} />
-		<CardBody className="p-2">
-			<div>SVG {shortName} is {printer.prettyInt(bytes)} bytes.</div>
-			<div>TODO recompress</div>
-		</CardBody>
+	<CardHeader>Optimise SVG Image</CardHeader>
+	<img className="preview img-preview p-1" src={url} />
+	<CardBody className="p-2">
+		<div>Original SVG image is {printer.prettyInt(bytes)} bytes.</div>
+		{optSpec ? <Improvement origSpec={spec} optSpec={optSpec} /> : <Misc.Loading text="Optimising SVG image..." />}
+	</CardBody>
+	</Card>;
+}
+
+export function ScriptRecommendation({spec}) {
+	const { shortName, bytes, url } = spec;
+	const [optSpec, setOptSpec] = useState();
+
+	useEffect(() => {
+		ServerIO.load(RECOMPRESS_ENDPOINT, { data: { url, type: 'script' } }).then(res => {
+			setOptSpec({ ...res.data });
+		});
+	}, [url]);
+
+	return <Card className="opt-rec script-rec">
+	<CardHeader>Optimise JavaScript</CardHeader>
+	<p className="preview script-preview p-1">
+		<a href={url}>{shortName}</a>
+	</p>
+	<CardBody className="p-2">
+		<div>Original JavaScript file is {printer.prettyInt(bytes)} bytes.</div>
+		{optSpec ? <Improvement origSpec={spec} optSpec={optSpec} /> : <Misc.Loading text="Minifying JavaScript..." />}
+	</CardBody>
 	</Card>;
 }
 
@@ -106,7 +153,7 @@ const recComponents = {
 	font: FontRecommendation,
 	gif: GifRecommendation,
 	svg: SvgRecommendation,
-	// script: ScriptRecommendation
+	script: ScriptRecommendation
 };
 
 
