28f876b2aa7f78444fecf725ecfc9e9ea498909d
Roscoe <roscoe@good-loop.com>
2023-11-23 17:22:05 +0000

    PropControlCode: simpler flicker-free highlighting + add auto-formatting button

--- base/components/propcontrols/PropControlCode.jsx
+++ base/components/propcontrols/PropControlCode.jsx
@@ -1,150 +1,209 @@
 import React, { useEffect, useRef, useState } from 'react';
+// TODO Maybe auto-load this from CDN like with Beautify?
 import Prism from 'prismjs';
 Prism.manual = true; // Suppress Prism auto-highlighting
 
-import { FakeEvent, registerControl } from '../PropControl';
+import { registerControl } from '../PropControl';
 import { setInputValue } from '../../utils/miscutils';
 
 // import '../../style/prism-dark.less';
 import '../../style/prism-tomorrow.less';
 import '../../style/PropControls/PropControlCode.less';
+import { Button } from 'reactstrap';
 
-/** Used as a "split tree here when flattening" marker when turning */
-const NEWLINE = Symbol();
 
+
+
+/** Unused: verifies the line splitter's ability to work with nested tokens */
 const multilineTest = [
 	'This is a text token\n',
-	{ type: 'important', content: ['Multi-string text token 1 ', 'Multi-string text token 2', 'Multi-string text token 3'] },
+	{ type: 'important', content: ['Multi-string text token 1 ', 'Multi-string text token 2 ', 'Multi-string text token 3'] },
 	'\n',
 	{ type: 'property', content: 'Single string which spans multiple lines\nContinuation of multiline string' },
 	'\n',
 	{ type: 'comment', content: [
 		'Comment token 1',
 		{ type: 'selector', content: 'Comment+selector nested token' },
+		' ',
 		{ type: 'property', content: 'Comment+property nested MULTILINE token line 1\nComment+property nested MULTILINE token line 2' },
+		'\n',
 		'Comment token end line 1\nComment token end line 2',
 	]},
 ];
 
 
-/** Recursively render a Prism.js Token to a <span> */
-function TokenSpan({ token }) {
-	if (typeof token === 'string') return token;
-	if (token.map) return token.map((tkn, i) => <TokenSpan key={i} token={tkn} />);
-	return <span className={`token ${token.type}`}><TokenSpan token={token.content} /></span>;
-}
+/**
+ * Where a token's content is an array, e.g. ['\nLine1\nLine2', 'MoreLine2\n', 'Line3\n']
+ * splitToken yields an array of arrays where:
+ * - every element boundary in an INNER array is a line break
+ * - but the OUTER element boundaries are just token breaks
+ * e.g. [['', 'Line1', 'Line2'], ['MoreLine2', ''], ['Line3', '']]
+ * Regroup the array so that's reversed - outer boundaries are line breaks, inners are token breaks.
+ * e.g. [[''], [Line1], ['Line2', 'MoreLine2'], ['', 'Line3'], ['']]
+*/
+const regroupLines = (notLines) => (
+	notLines.reduce((lines, tknLines) => {
+		if (!tknLines.length) return lines; // Ignore empty tokens
+		if (!lines.length) lines.push([]); // Make sure there's a container to start first line
+		// attach first item to end of current line
+		lines.lastItem.push(tknLines[0]);
+		// remaining items are all additional lines
+		for (let i = 1; i < tknLines.length; i++) { lines.push([tknLines[i]]); }
+		return lines;
+	}, [])
+);
 
 
 /**
- * Split an array on a particular item and group the runs of elements between the split points.
- * [1, splitOn, 2, 3, splitOn, 4, [5, 6]] --> [[1], splitOn, [2, 3], splitOn, [4, [5, 6]]]
- * @param {*[]} array The input array
- * @param {*} splitOn Array is split where element === splitOn
- * @param {?boolean} [keep=true] Keep the splitOn elements in between groups
+ * Process a Prism.js token (or array thereof) and:
+ * - Split the token stream on every newline
+ * - Replace all non-whitespace characters with spaces
+ * So e.g. { type: "x", content: { type: "y", content: "Line 1\n\tLine 2" } }
+ * ...becomes [
+ *   { type: "x", content: { type: "y", content: "      " } },
+ *   { type: "x", content: { type: "y", content: "\t      " } }
+ * ]
+ * Q: Why do this and not just put the tokens in a preformatted block?
+ * A: Well, no reason right now - but this parsing can enable things like line numbering with a little more poking.
+ * @param {Prism.Token|Prism.Token[]} tokens As output from Prism.tokenize
+ * @returns {Prism.Token[]} Split and regrouped into an array of lines
  */
-const groupBetweenSplits = (array, splitOn, keep = true) => {
-	const groups = [];
-	let group = [];
-	const last = array.length - 1;
-	array.forEach((el, i) => {
-		const isSplitter = (el === splitOn);
-		if (isSplitter) {
-			groups.push(group);
-			if (keep) groups.push(el);
-			group = [];
-		} else {
-			group.push(el);
-			if (i === last) groups.push(group);
-		}
-	});
-	return groups;
-};
-
-
-/**
- * Process a Prism.js token and do two things:
- * - Wherever there's a newline, split the token stream (including nesting) and insert a NEWLINE symbol, so e.g.:
- * -- { type: "x", content: { type: "y", content: "Line 1\nLine 2" } } becomes:
- * -- [{ type: "x", content: { type: "y", content: "Line 1" } }, NEWLINE, { type: "x", content: { type: "y", content: "Line 1" } }]
- * - Augment it with flattened text, which can be used at the top level as a per-line key.
- * @param {Prism.Token} token
- * @returns {(Prism.Token|NEWLINE)[]}
- */
-const splitToken = (token) => {
+const splitTokenLines = (tokens) => {
+	// Trivial case: no input, empty output.
+	if (!tokens) return [];
 	// Base case: split string on newline character (LF or CRLF - can use /\r\n|\r|\n/ if we somehow need to support MacOS 9)
-	// 'Hello\nWorld\n' --> ['Hello', LINEBREAK, 'world', LINEBREAK, '']
-	// Don't trim off empty strings - they'll either get merged and disappear, or (correctly) become empty <li>s at the root of the tree.
-	if (typeof token === 'string') return token.split(/\r?\n/).flatMap(a => [a, NEWLINE]).slice(0, -1);
-	const { content, ...rest } = token;
-	// Recurse in until we reach strings, then group everything between NEWLINEs into arrays
-	const splitChildren = content.flatMap?.(splitToken) || splitToken(content);
-	return groupBetweenSplits(splitChildren, NEWLINE).map(group => {
-		if (group === NEWLINE || !group.length) return group;
-		const text = group.reduce((acc, el) => acc + (typeof el === 'string' ? el : el.text), '');
-		return { text, content: group, ...rest };
-	});
+	// 'Hello\nWorld\n' --> ['Hello', 'world', ''] --> ['     ', '     ', '']
+	// Don't trim off empty strings! They signify leading and trailing newlines.
+	if (typeof tokens === 'string') return tokens.split(/\r?\n/).map(s => s.replace(/\S/g, ' '));
+
+	// Array case: Split all tokens into sets of lines, then regroup to merge non-linebreak token boundaries.
+	if (tokens.map) return regroupLines(tokens.map(splitTokenLines));
+
+	// Recursive case: Process token's content to lines, then wrap each line in a copy of the token.
+	return splitTokenLines(tokens.content).map(content => ({...tokens, content}));
 };
 
 
-const tokensToLines = (tokens = []) => {
-	const lineSpecs = groupBetweenSplits(tokens.flatMap(splitToken), NEWLINE, false);
-	const seenKeys = {};
-	return lineSpecs.map((ls, i) => {
-		let { text } = ls;
-		if (seenKeys[text]) text += `-${i}`;
-		seenKeys[text] = true;
-		return <li key={text}><TokenSpan token={ls} /></li>
-	});
+/** Recursively render a Prism.js Token or array of tokens to <span>s */
+function RenderToken({ token }) {
+	if (typeof token === 'string') return token;
+	if (token.map) return token.map((tkn, i) => <RenderToken key={i} token={tkn} />);
+	return <span className={`token ${token.type}`}><RenderToken token={token.content} /></span>;
 }
 
 
-const PropControlCode = ({ storeValue, prop, path, lang, onChange, onKeyDown, rawValue, setRawValue, ...otherStuff }) => {
-	const [tokenLines, setTokenLines] = useState();
+/** URL to language-specific Beautify bundle */
+const beautifyUrl = lang => `https://cdnjs.cloudflare.com/ajax/libs/js-beautify/1.14.11/beautify${lang}.min.js`;
 
-	const codeRef = useRef(); // The editable <code>
 
-	// Update highlighting in <code> element
-	const doHighlight = () => {
-		const tokens = Prism.tokenize(codeRef.current.innerText, Prism.languages.css);
-		setTokenLines(tokensToLines(tokens));
+/** Languages Beautify can autoformat for us */
+const beautifyUrls = {
+	js: beautifyUrl(''),
+	css: beautifyUrl('-css'),
+	html: beautifyUrl('-html')
+};
+
+
+/**
+ * Automatically format code using beautify.js
+ * Loads the library from CDN on first run to avoid yet another dependency in the bundle.
+ * @param {String} lang "js", "css", "html"
+ * @param {String} text Input code. JS and CSS will have all linebreaks collapsed and rebuilt.
+ * @param {Function} callback Called with the formatted text when done (may be inline or async depending on whether library is already loaded)
+ */
+const formatCode = (lang, text, callback) => {
+	const scriptUrl = beautifyUrls[lang];
+	let beautifyScript = document.querySelector(`script[src="${scriptUrl}"]`);
+	if (!beautifyScript) {
+		beautifyScript = document.createElement('script');
+		beautifyScript.src = scriptUrl;
+		document.head.appendChild(beautifyScript);
+	}
+	// Which Beautify function do we call?
+	const fnName = `${lang}_beautify`;
+
+	const doFormat = () => {
+		const options = { indent_with_tabs: true, css: { selector_separator_newline: false, preserve_newlines: false } };
+		// HTML linebreaks are important so we preserve them - CSS/JS we collapse and rebuild
+		text = (lang === 'html') ? text : text.replaceAll(/\r?\n/g, ' ');
+		const formatted = window[fnName](text, options);
+		callback(formatted)
 	};
+	if (!window[fnName]) {
+		beautifyScript.addEventListener('load', doFormat);
+	} else {
+		doFormat();
+	}
+};
 
-	// Highlight the <code> element as soon as it mounts
+
+/**
+ * Basically just a <textarea> - but with two extra features:
+ * - A non-interactable overlay which paints syntax highlighting on the text
+ * - A button for auto-formatting the text per JS, CS or HTML rules
+ * @param {Object} p
+ * @param {String} p.lang Language to highlight as - eg. "css", "js", "html"
+*/
+const PropControlCode = ({ lang, onChange, onKeyDown, rawValue }) => {
+	const inputRef = useRef(); // The textarea
+	const highlightRef = useRef(); // The highlighting overlay
+
+	// When text changes, regenerate highlighting overlay
+	const [tokenLines, setTokenLines] = useState([]);
 	useEffect(() => {
-		if (codeRef.current) doHighlight();
-	}, [codeRef.current])
+		const tokens = Prism.tokenize(rawValue || '', Prism.languages.css);
+		setTokenLines(splitTokenLines(tokens));
+	}, [rawValue]);
 
-	// Event handlers for <code>
+	// Event handlers for <textarea> input
 	const inputEvents = {
-		onKeyDown: (e) => {
-			onKeyDown && onKeyDown(e);
-			if (e.key !== 'Tab') return;
-			if (!codeRef.current) return;
-
-			// Tab should insert tab character instead of selecting next interactable element
-			const code = codeRef.current;
-			const sel = window.getSelection();
-			const isInside = (code.contains(sel.anchorNode) && code.contains(sel.focusNode));
-			if (!isInside) return; // Don't act on a selection that isn't fully inside the editor!
-			e.preventDefault();
-			// Replace the selection with a tab character
-			sel.deleteFromDocument();
-			sel.getRangeAt(0).insertNode('\t');
-			onChange && onChange(new FakeEvent(codeRef.innerText));
-			doHighlight();
-			// ...and push caret forward by 1??
+		onChange,
+		// Match scrolling between textarea and highlighter
+		onScroll: () => {
+			if (!inputRef.current || !highlightRef.current) return;
+			highlightRef.current.scrollLeft = inputRef.current.scrollLeft;
+			highlightRef.current.scrollTop = inputRef.current.scrollTop;
 		},
-		onInput: () => {
-			onChange && onChange(new FakeEvent(codeRef.innerText));
-			doHighlight();
+		// Catch "Tab" keypresses and insert a \t instead of selecting next interactable element
+		onKeyDown: (e) => {
+			if (onKeyDown && !onKeyDown(e)) return; // Don't handle if custom handler returns false
+			if (e.key !== 'Tab') return;
+			if (!inputRef.current) return;
+			e.preventDefault();
+			// Clean way: use fake user-input API (also pushes onto the undo stack)
+			if (!document.execCommand('insertText', false, '\t')) {
+				// Fallback (doesn't support undo): Overwrite selection, trigger onChange, push caret forward by 1.
+				const { selectionStart: ss, selectionEnd: se, value } = inputRef.current;
+				setInputValue(inputRef.current, value.slice(0, ss) + '\t' + value.slice(se));
+				inputRef.current.setSelectionRange(ss + 1, ss + 1);
+			}
 		}
 	};
 
-	return <pre className={`language-${lang}`} {...otherStuff}>
-		<code className={`language-${lang}`} {...inputEvents} contentEditable="plaintext-only" ref={codeRef}>
-			<ol>{tokenLines || storeValue}</ol>
-		</code>
-	</pre>;
+	// Is this a language we can auto-format?
+	const doFormat = beautifyUrls[lang] && (() => {
+		const callback = formatted => setInputValue(inputRef.current, formatted);
+		formatCode(lang, rawValue, callback);
+	});
+
+	return <>
+		<pre className={`language-${lang}`}>
+			<code className={`language-${lang}`}>
+				<textarea value={rawValue} ref={inputRef} {...inputEvents} rows={10} />
+				<div className="highlighter" ref={highlightRef}>
+					{tokenLines.map((line, i) => (
+						// The <wbr> is a zero-width space that ensures empty lines still occupy height
+						<div key={i}><wbr /><RenderToken token={line} /></div>
+					))}
+				</div>
+			</code>
+		</pre>
+		{doFormat && (
+			<Button className="mt-1 pull-right" onClick={doFormat}>
+				Clean up formatting
+			</Button>
+		)}
+	</>;
 };
 
 
